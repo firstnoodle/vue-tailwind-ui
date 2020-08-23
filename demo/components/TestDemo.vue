09e04e71b50b741764bb0596143a8267554b4175
sebastian thielke <seb@firstnoodle.io>
2020-08-23 14:43:52 +0200

    Created Test and TestDemo components

---
+++ demo/components/TestDemo.vue
@@ -0,0 +1,12 @@
+<template>
+   <test message="yourself!"/> 
+</template>
+
+<script>
+import Test from '~/components/Test';
+
+export default {
+    name: "TestDemo",
+    components: { Test } 
+}
+</script>
