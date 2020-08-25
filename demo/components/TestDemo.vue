44d1bd8acead27f79d6c4437334d379f5050fcad
sebastian thielke <seb@firstnoodle.io>
2020-08-25 10:19:02 +0200

    Updated TestDemo.vue with parent div that has theme class

--- demo/components/TestDemo.vue
+++ demo/components/TestDemo.vue
@@ -1,8 +1,14 @@
 <template>
-   <test message="yourself!"/> 
+    <div class="theme-light">
+        <test message="yourself!"/> 
+    </div>
 </template>
 
 <script>
+// Remember to import global css in every demo
+import '~/css/main.css';
+
+// Import the component we want to demo
 import Test from '~/components/Test';
 
 export default {
