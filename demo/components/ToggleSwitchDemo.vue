9dcca6d7ef4af570d991198324593c01eaa1d269
sebastian thielke <seb@firstnoodle.io>
2020-10-03 22:56:23 +0200

    Create ToggleSwitchDemo.vue

---
+++ demo/components/ToggleSwitchDemo.vue
@@ -0,0 +1,21 @@
+<template>
+    <demo-wrapper>
+        <h1 class="mb-4 text-xl font-bold">ToggleSwitch</h1>
+        <toggle-switch :value="enabled" @toggle="enabled = !enabled" />
+        <div class="mt-2">value: {{ enabled }}</div>
+    </demo-wrapper> 
+</template>
+
+<script>
+import DemoWrapper from './DemoWrapper';
+import ToggleSwitch from '~/components/ToggleSwitch';
+export default {
+    name: 'SwitchDemo',
+    components: { DemoWrapper, ToggleSwitch },
+    data() {
+        return {
+            enabled: true,
+        }
+    }
+}
+</script>
