ca414361defeb7877a42091f093ca58bdf662b6e
sebastian thielke <seb@firstnoodle.io>
2020-10-03 23:09:32 +0200

    Create ListItemDemo.vue

---
+++ demo/ListItemDemo.vue
@@ -0,0 +1,20 @@
+<template>
+    <demo-wrapper title="ListItem demo">
+        <list-item>
+            listing
+        </list-item>
+    </demo-wrapper> 
+</template>
+
+<script>
+import DemoWrapper from './DemoComponents/DemoWrapper';
+import ListItem from '~/components/ListItem';
+export default {
+    name: 'ListItemDemo',
+    components: { DemoWrapper, ListItem },
+    data() {
+        return {
+        }
+    }
+}
+</script>
