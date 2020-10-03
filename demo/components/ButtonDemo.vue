129ed6e5fdc8e34d166ad02a6277dcd1fa33e168
sebastian thielke <seb@firstnoodle.io>
2020-10-03 22:54:38 +0200

    Create ButtonDemo.vue

---
+++ demo/components/ButtonDemo.vue
@@ -0,0 +1,13 @@
+<template>
+    <demo-wrapper>
+        Button demo
+    </demo-wrapper> 
+</template>
+
+<script>
+import DemoWrapper from './DemoWrapper';
+export default {
+    name: 'ButtonDemo',
+    components: { DemoWrapper }
+}
+</script>
