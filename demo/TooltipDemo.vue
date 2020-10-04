9ee0d212c281e3fd572a35adbfb4bb39043c9623
sebastian thielke <seb@firstnoodle.io>
2020-10-04 21:24:48 +0200

    Imported Tooltip and created TooltipDemo

---
+++ demo/TooltipDemo.vue
@@ -0,0 +1,21 @@
+<template>
+    <demo-wrapper title="Tooltip">
+        <tooltip text="Now you know me" >
+            <span>Hover me</span>
+        </tooltip>
+    </demo-wrapper> 
+</template>
+
+<script>
+import DemoWrapper from './DemoComponents/DemoWrapper';
+import Tooltip from '~/components/Tooltip';
+export default {
+    name: 'TooltipDemo',
+    components: { DemoWrapper, Tooltip },
+    data() {
+        return {
+            enabled: true,
+        }
+    }
+}
+</script>
---
+++ src/components/Tooltip.js
@@ -0,0 +1,54 @@
+import Vue from 'vue';
+import BasePopper from './BasePopper.vue';
+
+export default Vue.component('tooltip', {
+    components: { BasePopper },
+    props: {
+        text: {
+            type: String,
+            required: true
+        },
+        bgcolor: {
+            type: String,
+            default: 'black'
+        },
+        textcolor: {
+            type: String,
+            default: 'white'
+        }
+    },
+
+    render(createElement) {
+        const tooltipLabelElement = createElement('span', { attrs: { class: `text-${this.textcolor}` } }, this.text);
+
+        const tooltipAttrs = { class: `popper px-2 py-1 rounded bg-${this.bgcolor} text-${this.bgcolor} text-xs shadow-md` };
+        const tooltipElement = createElement('div', { attrs: tooltipAttrs }, [tooltipLabelElement]);
+
+        const referenceElement = createElement('span', { slot: 'reference', attrs: { class: 'cursor-default' } }, this.$slots.default);
+
+        const popperOptions = {
+            attrs: {
+                class: 'tooltip'
+            },
+            props: {
+                trigger: "hover",
+                appendToBody: true,
+                visibleArrow: true,
+                rootClass: 'tooltip',
+                options: {
+                    placement: 'top',
+                    modifiers: [
+                        {
+                            name: 'offset',
+                            options: {
+                                offset: [0, 8]
+                            },
+                        }
+                    ]
+                }
+            }
+        };
+
+        return createElement('base-popper', popperOptions, [referenceElement, tooltipElement]);
+    }
+});
