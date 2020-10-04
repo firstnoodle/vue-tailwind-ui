ac97a7bdf74d3b25fd05d0103e22e924efb8eae0
sebastian thielke <seb@firstnoodle.io>
2020-10-04 20:49:37 +0200

    Imported CheckBox component and created CheckBoxDemo

---
+++ demo/CheckBoxDemo.vue
@@ -0,0 +1,19 @@
+<template>
+    <demo-wrapper title="CheckBox">
+        <check-box :checked="enabled" :label="enabled" @click="enabled = !enabled" />
+    </demo-wrapper> 
+</template>
+
+<script>
+import DemoWrapper from './DemoComponents/DemoWrapper';
+import CheckBox from '~/components/CheckBox';
+export default {
+    name: 'SwitchDemo',
+    components: { DemoWrapper, CheckBox },
+    data() {
+        return {
+            enabled: true,
+        }
+    }
+}
+</script>
---
+++ src/components/CheckBox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex items-center" @click="$emit('click')">
+        <input
+            v-model="checked"
+            type="checkbox"
+            :name="label"
+            class="form-checkbox h-4 w-4 text-indigo-600 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+        />
+        <label :for="label" class="ml-2 block text-sm leading-5 text-gray-900">{{ label }}</label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CheckBox",
+    props: {
+        checked: {
+            type: Boolean,
+            required: true
+        },
+        label: {
+            type: String,
+            required: false
+        }
+    }
+};
+</script>
