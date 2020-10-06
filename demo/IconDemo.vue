5206dd776ef4fef1fbe36ab4fd832527a28798d4
sebastian thielke <seb@firstnoodle.io>
2020-10-06 15:57:01 +0200

    Created IconDemo

---
+++ demo/IconDemo.vue
@@ -0,0 +1,26 @@
+<template>
+    <demo-wrapper title="Icons">
+        <span class="p-2">
+            <icon value="user" />
+        </span>
+        <span class="p-2">
+            <icon value="envelope" />
+        </span>
+        <span class="p-2">
+            <icon value="trend" />
+        </span>
+        <span class="p-2">
+            <icon value="sun" />
+        </span>
+    </demo-wrapper> 
+</template>
+
+<script>
+import DemoWrapper from './DemoComponents/DemoWrapper';
+import Icon from '~/components/Icon';
+
+export default {
+    name: 'ButtonDemo',
+    components: { DemoWrapper, Icon }
+}
+</script>
