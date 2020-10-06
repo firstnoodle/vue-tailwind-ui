169db6e303c89e7750fe4a21d08ea55195a65438
sebastian thielke <seb@firstnoodle.io>
2020-10-06 20:46:45 +0200

    Created IconButton and IconButtonDemo

---
+++ demo/IconButtonDemo.vue
@@ -0,0 +1,21 @@
+<template>
+    <demo-wrapper title="IconButton demo">
+        <icon-button value="edit" @click="onClick"/>
+        <icon-button value="trash" @click="onClick"/>
+    </demo-wrapper> 
+</template>
+
+<script>
+import DemoWrapper from './DemoComponents/DemoWrapper';
+import IconButton from '~/components/IconButton';
+
+export default {
+    name: 'ButtonDemo',
+    components: { DemoWrapper, IconButton },
+    methods: {
+        onClick() {
+            console.log('click');
+        }
+    }
+}
+</script>
---
+++ src/components/IconButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <div @click="$emit('click')" class="w-8 h-8 inline-flex items-center text-action hover:text-action justify-center rounded-full hover:bg-blue-100 cursor-pointer">
+        <icon :value="value" />
+    </div>
+</template>
+
+<script>
+import Icon from './Icon';
+export default {
+    name: 'IconButton',
+    components: { Icon },
+    props: {
+        value: {
+            type: String,
+            default: 'index-finger-up'
+        }
+    }
+}
+</script>
