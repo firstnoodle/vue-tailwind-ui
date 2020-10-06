06a45dbff0cb3d60e9620caeb440f1c73860f0ca
sebastian thielke <seb@firstnoodle.io>
2020-10-06 15:55:57 +0200

    Imported Icon component

---
+++ src/components/Icon.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+
+export default Vue.component('icon', {
+    // functional: true,
+    props: {
+        value: {
+            type: String,
+            default: 'index-finger-up'
+        },
+        color: {
+            type: String,
+            default: ''
+        },
+        size: {
+            type: String,
+            default: ''
+        }
+    },
+    render(createElement) {
+        const size = this.size ? ` text-${this.size}` : '';
+        const color = this.color ? ` text-${this.color}` : '';
+        return createElement(
+            'span', 
+            { class: `icon icon-${this.value}${color}${size}`}
+        );
+    }
+});
