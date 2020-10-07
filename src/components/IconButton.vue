31ba8486c7d2f065270b0ce2f3ae551de2360e15
sebastian thielke <seb@firstnoodle.io>
2020-10-07 09:48:10 +0200

    IconButton → added tabindex

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -1,5 +1,8 @@
 <template>
-    <div @click="$emit('click')" class="w-8 h-8 inline-flex items-center text-action hover:text-action justify-center rounded-full hover:bg-blue-100 cursor-pointer">
+    <div 
+        @click="$emit('click')" 
+        tabindex="0"
+        class="w-8 h-8 inline-flex items-center text-action hover:text-action justify-center rounded-full hover:bg-blue-100 focus:bg-blue-200 focus:outline-none cursor-pointer">
         <icon :value="value" />
     </div>
 </template>
