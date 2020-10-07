67b14b15912f6414d80cb09e52690cf8e2ac544e
sebastian thielke <seb@firstnoodle.io>
2020-10-07 23:01:30 +0200

    Checkbox → now use a button with svg checkmark

--- src/components/CheckBox.vue
+++ src/components/CheckBox.vue
@@ -1,27 +1,25 @@
 <template>
-    <div class="flex items-center" @click="$emit('click')">
-        <input
-            v-model="checked"
-            type="checkbox"
-            :name="label"
-            class="form-checkbox h-4 w-4 text-indigo-600 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-        />
-        <label :for="label" class="ml-2 block text-sm leading-5 text-primary">{{ label }}</label>
+    <div class="flex items-center">
+        <button :class="{ 'border-blue-700 bg-blue-500' : value }" class="flex items-center justify-center w-4 h-4 border border-default rounded focus:border-blue-600 focus:outline-none focus:shadow-outline" @click="$emit('click')">
+            <svg v-if="value" width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8.41166 0.5L10 2.0417L4.01353 7.5L0 3.57411L1.60557 2.04932L4.02897 4.39988L8.41166 0.5Z" fill="white"/>
+            </svg>
+        </button>
+        <span v-if="label" class="ml-2 block text-sm leading-5 text-primary">{{ label }}</span>
     </div>
 </template>
 
 <script>
 export default {
-    name: "CheckBox",
+    name: 'Checkbox',
     props: {
-        checked: {
-            type: Boolean,
-            required: true
-        },
         label: {
             type: String,
-            required: false
+            default: null
+        },
+        value: {
+            type: Boolean
         }
     }
-};
+}
 </script>
