b83ecdd3899d6c95f116a34ba24b44dfa6c8b262
sebastian thielke <seb@firstnoodle.io>
2020-10-07 23:02:28 +0200

    Update CheckBoxDemo.vue

--- demo/CheckBoxDemo.vue
+++ demo/CheckBoxDemo.vue
@@ -1,15 +1,16 @@
 <template>
     <demo-wrapper title="CheckBox">
-        <check-box :checked="enabled" :label="enabled" @click="enabled = !enabled" />
+        <!-- <check-box :checked="enabled" :label="enabled" @click="enabled = !enabled" /> -->
+        <checkbox :value="enabled" @click="enabled = !enabled" />
     </demo-wrapper> 
 </template>
 
 <script>
 import DemoWrapper from './DemoComponents/DemoWrapper';
-import CheckBox from '~/components/CheckBox';
+import Checkbox from '~/components/Checkbox';
 export default {
     name: 'SwitchDemo',
-    components: { DemoWrapper, CheckBox },
+    components: { DemoWrapper, Checkbox },
     data() {
         return {
             enabled: true,
