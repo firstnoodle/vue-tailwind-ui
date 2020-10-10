ccfe5e01f5768d6b46e859e549cd1ee904b9a56d
sebastian thielke <seb@firstnoodle.io>
2020-10-10 22:47:16 +0200

    Too big commit.. Implemented select + a lot more

--- demo/ListItemDemo.vue
+++ demo/ListItemDemo.vue
@@ -1,7 +1,10 @@
 <template>
     <demo-wrapper title="ListItem demo">
+        <div v-if="computedSelectCount" class="">
+            <base-button size="xs" icon="trash" />
+        </div>
         <draggable 
-            v-model="items" 
+            v-model="auditRequirementsTable" 
             v-bind="dragOptions"
             handle=".drag-handle"
             @start="drag = true"
@@ -9,21 +12,49 @@
             >
             <transition-group type="transition">
                 <list-item 
-                    v-for="item in items"
-                    :key="item.id"
+                    v-for="item in auditRequirementsTable"
+                    :key="item.listId"
                     :edit="item.edit"
                     :draggable="true" 
                     :selectable="true"
+                    :selected="item.selected"
+                    @select="onItemSelect(item.id)"
                     @delete="onItemDelete(item.id)"
                     >
                     {{ item.description }}
 
                     <template #edit>
-                        <div class="flex">
-                            Input
+                        <div class="flex mb-2">
+                            <fn-select 
+                                v-model="selectCurrentOption" 
+                                filterable
+                                focus-on-mounted
+                                :loading="selectLoading"
+                                loading-text="Searching.."
+                                :remoteMethod="selectFetchOptions"
+                                no-match-option-text="Create requirement"
+                                placeholder="Select requirement"
+                                @select="onSelectOption"
+                                @createNew="selectCreateNew"
+                                >
+                                    <fn-select-option 
+                                        v-for="option in selectOptions" 
+                                        :key="option.id" 
+                                        :label="option.label" 
+                                        :value="option"
+                                        :disabled="getOptionDisabledState(option)"
+                                    />
+                            </fn-select>
                         </div>
                         <div class="flex items-center space-x-2">
-                            <base-button @click="addNewItem" type="primary" icon="plus">Add request</base-button>
+                            <base-button 
+                                :loading="posting" 
+                                @click="saveNewItem" 
+                                type="primary"
+                                icon="plus"
+                            >
+                                Add request
+                            </base-button>
                             <base-button @click="cancelAddNewItem" plain type="primary">Cancel</base-button>
                         </div>
                     </template>
@@ -33,22 +64,26 @@
 
         <!-- Add new button -->
         <div class="mt-4" v-if="!computedEditState">
-            <base-button @click="createNewItem" plain type="primary" icon="plus">Add request</base-button>
+            <base-button @click="openNewItem" plain type="primary" icon="plus">Add request</base-button>
         </div>
     </demo-wrapper> 
 </template>
 
 <script>
+import jsonData from './data/requirements.json';
 import BaseButton from '~/components/BaseButton.js';
 import DemoWrapper from './DemoComponents/DemoWrapper';
 import draggable from 'vuedraggable';
+import FnSelect from '~/components/Select.js';
+import FnSelectOption from '~/components/SelectOption';
 import ListItem from '~/components/ListItem';
 
 export default {
     name: 'ListItemDemo',
-    components: { BaseButton, DemoWrapper, draggable, ListItem },
+    components: { BaseButton, DemoWrapper, draggable, FnSelect, FnSelectOption, ListItem },
     data() {
         return {
+            auditRequirementsTable: null,
             drag: false,
             dragOptions: {
                 animation: 200,
@@ -56,79 +91,132 @@ export default {
                 disabled: false,
                 ghostClass: "drag-ghost"
             },
-            items: [
-                {
-                    id: 1,
-                    selected: false,
-                    edit: false,
-                    description: 'List of deviations/NCs (excel file)',
-                },
-                {
-                    id: 2,
-                    selected: false,
-                    edit: false,
-                    description: 'List of change requests (excel file)',
-                },
-                {
-                    id: 3,
-                    selected: false,
-                    edit: false,
-                    description: 'List of recalls and potential recalls',
-                },
-                {
-                    id: 4,
-                    selected: false,
-                    edit: false,
-                    description: 'List of CAPA',
-                },
-                {
-                    id: 5,
-                    selected: false,
-                    edit: false,
-                    description: 'Presentation and minutes of the two latest QMRs',
-                },
-                {
-                    id: 6,
-                    selected: false,
-                    edit: false,
-                    description: 'Site Master File incl. drawing of the buildings in scope (layout/classification plan)',
-                },
-                {
-                    id: 7,
-                    selected: false,
-                    edit: false,
-                    description: 'Introduction to ongoing major projects',
-                },
-            ]
+            listIdIncrementor: 0,
+            posting: false,
+            requirementsTable: null,
+            selectCurrentOption: null,
+            selectLoading: false,
+            selectOptions: null,
         }
     },
     computed: {
         computedEditState() {
-            for(const item of this.items) {
+            for(const item of this.auditRequirementsTable) {
                 if(item.edit) return true;
             }
             return false;
+        },
+        computedSelectCount() {
+            let count = 0;
+            for(const item of this.auditRequirementsTable) {
+                if(item.selected) count++;
+            }
+            return count;
         }
     },
+    created() {
+        // The "database"
+        this.requirementsTable = jsonData.requirements;
+
+        this.auditRequirementsTable = this.requirementsTable
+            .filter(requirement => (requirement.default))
+            .map(requirement => {
+                return {
+                    id: requirement.id,
+                    listId: ++this.listIdIncrementor,
+                    description: requirement.description,
+                    edit: false,
+                    selected: false,
+                }
+            });
+
+    },
     methods: {
-        addNewItem() {
-            console.log('add new item');
+        saveNewItem() {                
+            this.posting = true;
+            setTimeout(() => {
+                this.auditRequirementsTable.forEach(option => {
+                    if(option.edit) {
+                        // TODO: id needs to be updated, but this way the ui glitches
+                        // solution: created view/list id for every item
+                        option.id = this.selectCurrentOption.value.id;
+                        option.description = this.selectCurrentOption.value.description;
+                        option.selected = false;
+                        option.edit = false;
+                    }
+                });
+                this.selectCurrentOption = null;
+                this.selectOptions = null;
+                this.posting = false;
+            }, 1000);
         },
 
         cancelAddNewItem() {
-            this.items = this.items.filter(item => !(item.edit));
+            this.selectCurrentOption = null;
+            this.selectOptions = null;
+            this.auditRequirementsTable = this.auditRequirementsTable.filter(item => !(item.edit));
         },
 
-        createNewItem() {
-            this.items.push({
-                id: Date.now(),
-                edit: true
+        openNewItem() {
+            this.auditRequirementsTable.push({
+                id: null,
+                listId: ++this.listIdIncrementor,
+                description: null,
+                edit: true,
+                selected: false
             });
         },
 
+        fakeApiCall(query) {
+            this.selectOptions = this.requirementsTable
+                .filter(requirement => {
+                    const targetString = requirement.description.toLowerCase();
+                    const queryString = query.toLowerCase();
+                    return targetString.includes(queryString);
+                })
+                .map(requirement => {
+                    return { 
+                        label: requirement.description, 
+                        value: requirement 
+                    }
+                });
+
+            this.selectLoading = false;
+        },
+
+        getOptionDisabledState(option) {
+            const ids = this.auditRequirementsTable.map(o => o.id);
+            const result = ids.includes(option.value.id);
+            return result;
+        },
+
         onItemDelete(id) {
-            this.items = this.items.filter(item => item.id !== id);
-        }
+            this.auditRequirementsTable = this.auditRequirementsTable.filter(item => item.id !== id);
+        },
+
+        onItemSelect(id) {
+            const target = this.auditRequirementsTable.find(requirement => requirement.id === id);
+            target.selected = !target.selected;
+        },
+
+        onSelectOption(option) {
+            this.selectCurrentOption = option;
+        },
+
+        selectCreateNew() {
+
+        },
+
+        selectFetchOptions(value, charsAdded) {
+            if(this.selectOptions !== null) { // did we make an API call ?
+                if(this.selectOptions.length === 0 && charsAdded > 0) { // do we have any results ?
+                    return;
+                }
+            }
+
+            this.selectLoading = true;
+            setTimeout(this.fakeApiCall.bind(null, value), 500);
+        },
     }
 }
 </script>
