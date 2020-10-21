1977c07b49d820c9f58b57b766c8eebfbc3d62f7
sebastian thielke <seb@firstnoodle.io>
2020-10-21 15:20:05 +0200

    MobileAppHeader → md:hidden

--- src/components/application/MobileAppHeader.vue
+++ src/components/application/MobileAppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="z-10 fixed w-screen top-0 left-0 flex items-center justify-between bg-white p-2 shadow">
+    <header class="md:hidden z-10 fixed w-screen top-0 left-0 flex items-center justify-between bg-white p-2 shadow">
         <div class="z-0 absolute flex items-center justify-center w-full h-full top-0 left-0 bg-transparent">
             <div class="flex items-center text-sm">
                 <slot name="center" />
