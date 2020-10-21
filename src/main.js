de3367106afb2cbbc4b7eefe519d7c5db661e54c
sebastian thielke <seb@firstnoodle.io>
2020-10-21 20:25:38 +0200

    main.js → import router

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import PortalVue from 'portal-vue'
 
+import router from '~/router/index';
+
 Vue.use(PortalVue);
 
 Vue.config.productionTip = false;
 
 new Vue({
+    router,
     render: h => h(App),
 }).$mount('#app');
