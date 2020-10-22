9874af64216b9e21e0fe30089a161a769dba7669
sebastian thielke <seb@firstnoodle.io>
2020-10-22 21:13:00 +0200

    Create SectionFooter.vue

---
+++ src/components/application/SectionFooter.vue
@@ -0,0 +1,11 @@
+<template functional>
+    <div class="flex items-center justify-between py-2 border-t border-brand">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SectionFooter',
+}
+</script>
