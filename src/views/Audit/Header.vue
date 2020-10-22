85c28ffd8347c05f060b17b3475145ceff280791
sebastian thielke <seb@firstnoodle.io>
2020-10-22 14:59:24 +0200

    Audit/Header → improved layout + tooltip on truncated Auditee

--- src/views/Audit/Header.vue
+++ src/views/Audit/Header.vue
@@ -6,22 +6,30 @@
             </router-link>
         </template>
         <template #main>
-            <div class="flex-1 flex items-center max-w-4xl m-auto">
-                <div class="flex items-center mr-8">
-                    <icon value="id" class="text-gray-600" /><span class="ml-2 text-sm font-light text-gray-800">AU1001234</span>
+            <div class="flex-1 flex items-center justify-between max-w-4xl m-auto">
+                <div class="flex items-center">
+                    <div class="flex items-center mr-4 lg:mr-8">
+                        <icon value="id" class="hidden lg:inline-block text-gray-600" />
+                        <span class="ml-2 text-sm font-light text-gray-800 truncate">AU1001234</span>
+                    </div>
+                    <div class="flex items-center mr-4 lg:mr-8">
+                        <icon value="calendar" class="hidden lg:inline-block text-gray-600" />
+                        <span class="ml-2 text-sm font-light text-gray-800 truncate">05 Apr 2020</span>
+                    </div>
+                    <div class="flex items-center mr-4 lg:mr-8">
+                        <icon value="authority" class="hidden lg:inline-block text-gray-600" />
+                        <span class="ml-2 text-sm font-light text-gray-800 truncate">NSTQ</span>
+                    </div>
+                    <tooltip text="Biopharm New Haemophilia API &amp; Biopharm QA API" placement="bottom" >
+                        <div class="flex items-center mr-4 lg:mr-8">
+                            <icon value="building" class="hidden lg:inline-block text-gray-600" />
+                            <span class="ml-2 text-sm font-light text-gray-800 truncate" style="max-width: 10rem">
+                                Biopharm New Haemophilia API & Biopharm QA API
+                            </span>
+                        </div>
+                    </tooltip>
                 </div>
-                <div class="flex items-center mr-8">
-                    <icon value="building" class="text-gray-600" /><span class="ml-2 text-sm font-light text-gray-800">CMC Bagsværd</span>
-                </div>
-                <div class="flex items-center mr-8">
-                    <icon value="calendar" class="text-gray-600" /><span class="ml-2 text-sm font-light text-gray-800">05 Apr 2020</span>
-                </div>
-                <div class="flex items-center mr-8">
-                    <icon value="authority" class="text-gray-600" /><span class="ml-2 text-sm font-light text-gray-800">NSTQ</span>
-                </div>
-            </div>
-            <div class="absolute right-0">
-                <base-button plain type="primary" icon="options" class="mr-2" />
+                <base-button plain type="primary" icon="document" class="self-end" />
             </div>
         </template>
     </view-header>
@@ -30,9 +38,10 @@
 <script>
 import BaseButton from '~/components/BaseButton';
 import Icon from '~/components/Icon';
+import Tooltip from '~/components/Tooltip';
 import ViewHeader from '~/components/application/ViewHeader';
 export default {
     name: 'Header',
-    components: { BaseButton, Icon, ViewHeader }
+    components: { BaseButton, Icon, Tooltip, ViewHeader }
 }
 </script>
