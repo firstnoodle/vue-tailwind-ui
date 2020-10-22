f99bb3b88a7a46580cea53680a3841ab4208c16b
sebastian thielke <seb@firstnoodle.io>
2020-10-22 13:01:14 +0200

    Audit/Nav → all wired up!

--- src/views/Audit/Nav.vue
+++ src/views/Audit/Nav.vue
@@ -1,34 +1,73 @@
 <template>
     <nav class="px-6 pb-6 pt-20 md:p-6">
-        <sidebar-nav-item href="#" icon="phase">Audit details</sidebar-nav-item>
+
+        <router-link :to="{ name: 'Details' }" tag="a" class="link">
+            <icon value="phase" />
+            <span class="ml-4 text-sm">Audit details</span>
+        </router-link>
 
         <div class="flex items-center py-2 mt-2">
             <span class="text-xs font-medium mr-2 text-gray-500">Planning</span>
             <span class="inline-block w-full border-b border-gray-400"></span>
         </div>
-        <sidebar-nav-item href="#" icon="building">Auditee</sidebar-nav-item>
-        <sidebar-nav-item href="#" icon="team">Audit team</sidebar-nav-item>
-        <sidebar-nav-item href="#" icon="index-finger-up">Criteria</sidebar-nav-item>
-        <sidebar-nav-item href="#" icon="scope">Scope</sidebar-nav-item>
-        <sidebar-nav-item href="#" icon="crosshair">Focus Areas</sidebar-nav-item>
-        <sidebar-nav-item href="#" icon="clock">Agenda</sidebar-nav-item>
-        <sidebar-nav-item href="#" icon="information">Info. Requests</sidebar-nav-item>
+        <router-link :to="{ name: 'Auditee' }" tag="a" class="link">
+            <icon value="building" />
+            <span class="ml-4 text-sm">Auditee</span>
+        </router-link>
+        <router-link :to="{ name: 'Team' }" tag="a" class="link">
+            <icon value="team" />
+            <span class="ml-4 text-sm">Audit team</span>
+        </router-link>
+        <router-link :to="{ name: 'Requirements' }" tag="a" class="link">
+            <icon value="index-finger-up" />
+            <span class="ml-4 text-sm">Requirements</span>
+        </router-link>
+        <router-link :to="{ name: 'Scope' }" tag="a" class="link">
+            <icon value="scope" />
+            <span class="ml-4 text-sm">Scope</span>
+        </router-link>
+        <router-link :to="{ name: 'Focus Areas' }" tag="a" class="link">
+            <icon value="crosshair" />
+            <span class="ml-4 text-sm">Focus Areas</span>
+        </router-link>
+        <router-link :to="{ name: 'Agenda' }" tag="a" class="link">
+            <icon value="clock" />
+            <span class="ml-4 text-sm">Agenda</span>
+        </router-link>
+        <router-link :to="{ name: 'Information Requests' }" tag="a" class="link">
+            <icon value="information" />
+            <span class="ml-4 text-sm">Info. Requests</span>
+        </router-link>
 
         <div class="flex items-center py-2 mt-2">
             <span class="text-xs font-medium mr-2 text-gray-500">Reporting</span>
             <span class="inline-block w-full border-b border-gray-400"></span>
         </div>
-        <sidebar-nav-item href="#" icon="index-finger-right">Findings</sidebar-nav-item>
-        <sidebar-nav-item href="#" icon="message">Suggestions</sidebar-nav-item>
-        <sidebar-nav-item href="#" icon="conclusion">Conclusion</sidebar-nav-item>
+        <router-link :to="{ name: 'Findings' }" tag="a" class="link">
+            <icon value="index-finger-right" />
+            <span class="ml-4 text-sm">Findings</span>
+        </router-link>
+        <router-link :to="{ name: 'Suggestions' }" tag="a" class="link">
+            <icon value="message" />
+            <span class="ml-4 text-sm">Suggestions</span>
+        </router-link>
+        <router-link :to="{ name: 'Conclusion' }" tag="a" class="link">
+            <icon value="conclusion" />
+            <span class="ml-4 text-sm">Conclusion</span>
+        </router-link>
     </nav>
 </template>
 
 <script>
-import SidebarNavItem from '~/components/application/SidebarNavItem';
-
 export default {
     name: 'Nav',
-    components: { SidebarNavItem }
 }
 </script>
+
+<style scoped>
+
+.link {
+    @apply flex items-center px-3 py-2 mb-1 bg-transparent text-gray-600 rounded-lg;
+}
+
+</style>
