6cd77f109a2aff1f72619c75ab9d400370997fce
sebastian thielke <seb@firstnoodle.io>
2020-10-24 10:34:11 +0200

    NavItem → deleted unused code

--- src/components/application/NavItem.vue
+++ src/components/application/NavItem.vue
@@ -29,16 +29,6 @@ export default {
             }
         }
     },
-    data() {
-        return {
-            classes: null,
-        }
-    },
-    created() {
-        this.classes = this.mobile 
-            ? 'flex items-center px-3 py-2 rounded-md text-sm text-gray-300 hover:text-gray-100 hover:bg-gray-800 focus:outline-none focus:text-white focus:bg-gray-700'
-            : 'flex items-center h-12 px-4 text-gray-600 hover:bg-black hover:text-white';
-    }
 }
 </script>
 
