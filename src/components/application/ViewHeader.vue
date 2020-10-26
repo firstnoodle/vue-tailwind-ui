c76049019d04748b771d036ad60618273c302fb9
sebastian thielke <seb@firstnoodle.io>
2020-10-26 18:27:52 +0100

    ViewHeader → added surrounding div

--- src/components/application/ViewHeader.vue
+++ src/components/application/ViewHeader.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="flex items-center max-w-8xl mx-auto h-12">
-        <div v-if="$slots.sidebar" class="flex-0 w-56 px-6 border-r border-gray-200">
-            <slot name="sidebar" />
-        </div>
-        <div class="relative flex-1 flex items-center px-4">
-            <slot name="main" />
+    <div class="h-12 w-full bg-white border-b border-subtle">
+        <div class="flex items-center max-w-8xl mx-auto h-12">
+            <div v-if="$slots.sidebar" class="flex-0 w-56 px-6 border-r border-subtle">
+                <slot name="sidebar" />
+            </div>
+            <div class="relative flex-1 flex items-center px-4">
+                <slot name="main" />
+            </div>
         </div>
     </div>
 </template>
