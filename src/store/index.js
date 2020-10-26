87053c347e5eb266f2580e06e94eb6aef949c172
sebastian thielke <seb@firstnoodle.io>
2020-10-26 18:29:46 +0100

    store/index → added actions, mutations, and getters + new state variables

--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,47 @@ import database from './modules/database';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+    actions: {
+        hideMobileMenu({ commit }) {
+            commit('SET_MOBILE_MENU_VISIBLE', false);
+        },
+        hideSidebar({ commit }) {
+            commit('SET_SIDEBAR_VISIBLE', false);
+        },
+        setMobile({ commit }, value) {
+            commit('SET_MOBILE', value);
+        },
+        showMobileMenu({ commit }) {
+            commit('SET_MOBILE_MENU_VISIBLE', true);
+        },
+        showSidebar({ commit }) {
+            commit('SET_SIDEBAR_VISIBLE', true);
+        },
+        toggleSidebar({ commit, state }) {
+            commit('SET_SIDEBAR_VISIBLE', !state.sidebarVisible);
+        }
+    },
+    mutations: {
+        SET_MOBILE(state, value) {
+            state.isMobile = value;
+        },
+        SET_MOBILE_MENU_VISIBLE(state, value) {
+            state.mobileMenuVisible = value;
+        },
+        SET_SIDEBAR_VISIBLE(state, value) {
+            state.sidebarVisible = value;
+        }
+    },
+    getters: {
+        sidebarVisible(state) {
+            return state.isMobile ? state.sidebarVisible : true;
+        }
+    },
     state: {
-        theme: 'light',
+        mobileMenuVisible: false,
+        isMobile: false,
+        sidebarVisible: false,
+        theme: 'theme-light',
     },
     modules: { database },
 });
