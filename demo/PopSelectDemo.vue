c66a2246d6b4283cb1b7abfec50c1ed2afe6a8d0
sebastian thielke <seb@firstnoodle.io>
2020-10-29 09:32:42 +0100

    PopSelectDemo → updated to match new api

--- demo/PopSelectDemo.vue
+++ demo/PopSelectDemo.vue
@@ -1,6 +1,16 @@
 <template>
-    <demo-wrapper title="Icons">
-        <pop-select />
+    <demo-wrapper title="PopSelect">
+        <pop-select @clear="onClear" clearSelectionText="No Focus Area" filterable>
+            <template #options>
+                <div class="w-full px-4 py-2 text-sm text-secondary font-light">Option 1</div>
+                <div class="w-full px-4 py-2 text-sm text-secondary font-light">Option 2</div>
+                <div class="w-full px-4 py-2 text-sm text-secondary font-light">Option 3</div>
+                <div class="w-full px-4 py-2 text-sm text-secondary font-light">Option 4</div>
+                <div class="w-full px-4 py-2 text-sm text-secondary font-light">Option 5</div>
+                <div class="w-full px-4 py-2 text-sm text-secondary font-light">Option 6</div>
+                <div class="w-full px-4 py-2 text-sm text-secondary font-light">Option 7</div>
+            </template>
+        </pop-select>
     </demo-wrapper> 
 </template>
 
@@ -10,6 +20,11 @@ import PopSelect from '~/components/PopSelect';
 
 export default {
     name: 'PopSelectDemo',
-    components: { DemoWrapper, PopSelect }
+    components: { DemoWrapper, PopSelect },
+    methods: {
+        onClear() {
+            console.log('clear selection');
+        }
+    }
 }
 </script>
