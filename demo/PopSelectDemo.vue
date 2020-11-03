23e8a03a0a5007701af6c890fa6c4dee01cd8539
sebastian thielke <seb@firstnoodle.io>
2020-11-03 14:03:07 +0100

    PopSelectDemo → new props + new data

--- demo/PopSelectDemo.vue
+++ demo/PopSelectDemo.vue
@@ -1,13 +1,15 @@
 <template>
     <demo-wrapper title="PopSelect">
-        <div>{{ selectedOption || 'none' }}</div>
         <pop-select 
             v-model="selectedOption"
             filterable
-            clearSelectionText="No Department" 
+            clearSelectionText="No Trend Category" 
+            icon="trend"
+            null-label="Set Trend Category"
             @change="onSelectChange"
             @clear="onClear" 
             >
+
             <template #options>
                 <pop-select-option 
                     v-for="option in options" 
@@ -31,13 +33,26 @@ export default {
     data() {
         return {
             options: [
-                {value: 1, label: 'Copenhagen'},
-                {value: 2, label: 'Suleymania'},
-                {value: 3, label: 'Banglore'},
-                {value: 4, label: 'Bogota'},
-                {value: 5, label: 'Casa Blanca'},
-                {value: 6, label: 'Mexico City'},
-                {value: 7, label: 'Nairobi'},
+                {value: 1, label: 'Quality Management System'},
+                {value: 2, label: 'Personnel'},
+                {value: 3, label: 'Premises'},
+                {value: 4, label: 'Documentation'},
+                {value: 5, label: 'Production'},
+                {value: 6, label: 'Production - Utilities'},
+                {value: 7, label: 'Production - API'},
+                {value: 8, label: 'Production - Wash and sterilisation'},
+                {value: 9, label: 'Production - Formulation'},
+                {value: 10, label: 'Production - Filling'},
+                {value: 11, label: 'Production - Inspection'},
+                {value: 12, label: 'Production - Packaging'},
+                {value: 13, label: 'Production - Storage and handling of materials'},
+                {value: 14, label: 'Quality Control'},
+                {value: 15, label: 'Outsourced Activities'},
+                {value: 16, label: 'Complaints and product recall'},
+                {value: 17, label: 'Internal Audit / Self Inspection'},
+                {value: 18, label: 'IT systems and Computerised equipment'},
+                {value: 19, label: 'Regulatory Activities'},
+                {value: 20, label: 'Medical Device Activities'},
             ],
             selectedOption: null,
         }
