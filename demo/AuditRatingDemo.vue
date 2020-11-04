cd72a67a61e7c2362f78330fcd882bcd4b542ef0
sebastian thielke <seb@firstnoodle.io>
2020-11-04 10:54:30 +0100

    Create AuditRatingDemo.vue

---
+++ demo/AuditRatingDemo.vue
@@ -0,0 +1,15 @@
+<template>
+    <demo-wrapper title="Audit Rating">
+        <audit-rating />
+    </demo-wrapper> 
+</template>
+
+<script>
+import DemoWrapper from './DemoComponents/DemoWrapper';
+import AuditRating from '~/components/AuditRating/main';
+
+export default {
+    name: 'AuditRatingDemo',
+    components: { AuditRating, DemoWrapper }
+}
+</script>
