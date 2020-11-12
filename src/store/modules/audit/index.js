6e36ceb2e58d7825b7472dda7f6810ff555d2b39
sebastian thielke <seb@firstnoodle.io>
2020-11-12 16:22:37 +0100

    views/FocusAreas → basic implementation of `list` module

--- src/store/modules/audit/index.js
+++ src/store/modules/audit/index.js
@@ -3,6 +3,7 @@ import listModule from '~/store/modules/list/index';
 export default {
     namespaced: true,
     modules: { 
+        focusAreas: listModule, 
         requirements: listModule, 
         team: listModule 
     }
--- src/views/Audit/views/FocusAreas.vue
+++ src/views/Audit/views/FocusAreas.vue
@@ -1,7 +1,76 @@
 <template>
     <view-content title="Focus Areas" icon="crosshair">
 
-        Content
+        <draggable 
+            v-model="focusAreas" 
+            v-bind="dragOptions"
+            handle=".drag-handle"
+            >
+            <transition-group type="transition">
+                <list-item 
+                    v-for="item in focusAreas"
+                    :key="item.listId"
+                    :draggable="true" 
+                    :edit="item.edit"
+                    @delete="$store.commit(`audits/${audit_id}/focusAreas/DELETE_ITEM`, item.id)"
+                    @select="onItemSelect(item.id)"
+                    class="last:mb-4"
+                    >
+                    <div class="inline-flex w-full">{{ item.name }}</div>
+
+                    <template #edit>
+                        <div class="flex pr-0 md:pr-16 mb-2">
+                            <div class="w-full">
+                                <fn-select 
+                                    v-model="selectedFocusAreaOption" 
+                                    :focus-after-select="false"
+                                    initial-text="Enter focus area"
+                                    :loading="focusAreaSelectLoading"
+                                    loading-text="Searching.."
+                                    placeholder="Select focus area"
+                                    ref="focusAreaSelect"
+                                    @select="onSelectFocusArea"
+                                    >
+                                        <fn-select-option 
+                                            v-for="focusArea in focusAreaOptions" 
+                                            :key="focusArea.id" 
+                                            :label="focusArea.label" 
+                                            :value="focusArea"
+                                            :disabled="getFocusAreaDisabledState(focusArea)"
+                                        />
+                                </fn-select>
+                            </div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <base-button 
+                                ref="saveButton"
+                                icon="plus"
+                                type="primary"
+                                :disabled="computedSaveButtonDisabled"
+                                :loading="posting" 
+                                @click.stop.prevent="saveItem" 
+                            >
+                                {{ 'Add focus area' }}
+                            </base-button>
+                            <base-button @click="cancelEditFocusArea(item)" plain type="primary">Cancel</base-button>
+                        </div>
+                    </template>
+                </list-item>
+            </transition-group>
+        </draggable>
+
+        <!-- Add new button -->
+        <div class="" v-if="showAddNewButton">
+            <base-button 
+                icon="plus"
+                plain 
+                ref="addNewButton"
+                type="primary" 
+                @click.stop.prevent="onOpenNewItem" 
+                >
+                Add focus area
+            </base-button>
+        </div>
 
         <template #footer>
             <view-content-footer-link :to="{ name: 'Audit scope' }" icon="scope" label="Scope" placement="left" />
@@ -11,11 +80,138 @@
 </template>
 
 <script>
+import BaseButton from '~/components/BaseButton.js';
+import draggable from 'vuedraggable';
+import FnSelect from '~/components/Select.js';
+import FnSelectOption from '~/components/SelectOption';
+import ListItem from '~/components/ListItem';
+
 import ViewContent from '~/components/application/ViewContent';
 import ViewContentFooterLink from '~/components/application/ViewContentFooterLink';
 
+import focusAreasTable from '~/../demo/data/focus_areas.js';
+
 export default {
-    name: 'Findings',
-    components: { ViewContent, ViewContentFooterLink }
+    name: 'Requirements',
+    components: { BaseButton, draggable, FnSelect, FnSelectOption, ListItem, ViewContent, ViewContentFooterLink },
+    data() {
+        return {
+            audit_id: null,
+            dragOptions: {
+                animation: 200,
+                group: "description",
+                disabled: false,
+                ghostClass: "drag-ghost"
+            },
+            posting: false,
+            selectedFocusAreaOption: null,
+            showAddNewButton: true,
+            focusAreaSelectLoading: false,
+            focusAreaOptions: null,
+        }
+    },
+    computed: {
+        computedSaveButtonDisabled() {
+            if(!this.selectedFocusAreaOption) return true;
+            return false;
+        },
+        focusAreas: {
+            get() {
+                return this.$store.state.audits[this.audit_id].focusAreas.items;
+            },
+            set(value) {
+                this.$store.commit(`audits/${this.audit_id}/focusAreas/UPDATE_ITEMS`, value)
+            }
+        }
+    },
+
+    created() {
+        this.audit_id = this.$route.params.id; 
+
+        const defaultFocusAreas = focusAreasTable
+            .map(focusArea => {
+                return {
+                    id: focusArea.id,
+                    listId: focusArea.id,
+                    name: focusArea.name,
+                    description: focusArea.description,
+                    edit: false,
+                    selected: false,
+                }
+            });
+
+        this.focusAreaOptions = focusAreasTable
+            .map(focusArea => {
+                return {
+                    label: focusArea.name,
+                    value: focusArea
+                }
+            });
+
+        this.$store.commit(`audits/${this.audit_id}/focusAreas/UPDATE_ITEMS`, defaultFocusAreas);
+    },
+
+    beforeDestroy() {
+        this.$store.dispatch(`audits/${this.audit_id}/focusAreas/cancelEditItems`);
+    },
+
+    methods: {
+        cancelEditFocusArea(item) {
+            if(!item) return;
+
+            this.selectedFocusAreaOption = null;
+            this.$store.dispatch(`audits/${this.audit_id}/focusAreas/cancelEditItem`, item);
+
+            // TODO: this prevents an unwanted ui glitch - $nextTick doesn't do the job...
+            setTimeout(() => this.showAddNewButton = true, 20);
+        },
+
+        getFocusAreaDisabledState(option) {
+            const ids = this.$store.state.audits[this.audit_id].focusAreas.items.map(item => item.id);
+            const result = ids.includes(option.value.id);
+            return result;
+        },
+
+        onOpenNewItem() {
+            this.$store.commit(
+                `audits/${this.audit_id}/focusAreas/ADD_ITEM`,
+                {
+                    name: null,
+                    description: null
+                }
+            );
+            this.showAddNewButton = false;
+            this.$nextTick(() => this.$refs.focusAreaSelect[0].focus());
+        },
+
+        onSelectFocusArea(option) {
+            this.selectedFocusAreaOption = option;
+
+            if(this.$refs.saveButton.length === 1) {
+                this.$nextTick(() => {
+                    this.$refs.saveButton[0].$el.focus();
+                });
+            } else {
+                // TODO: update
+                console.error('[ListItemDemo] too many or few items in $refs.saveButton array. Should only contain one');
+            }
+        },
+
+        saveItem() {                
+            this.posting = true;
+            setTimeout(() => {
+                this.$store.commit(`audits/${this.audit_id}/focusAreas/SAVE_ITEM`, { ...this.selectedFocusAreaOption.value });
+                this.selectedFocusAreaOption = null;
+                this.focusAreaOptions = null;
+                this.posting = false;
+
+                this.showAddNewButton = true;
+
+                this.$nextTick(() => {
+                    this.$refs.addNewButton.$el.focus();
+                });
+            }, 1000);
+        },
+    }
 }
 </script>
