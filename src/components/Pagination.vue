3383293356b25dd4e215ab0d3ea4ff297c357e6f
sebastian thielke <seb@firstnoodle.io>
2020-11-12 12:35:05 +0100

    WIP on implementing theme

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -5,17 +5,17 @@
             href="#"
             rel="prev" 
             aria-label="previous page" 
-            class="relative inline-flex items-center px-2 py-2 border border-granite-grey-15 bg-white text-sm leading-tight font-medium text-gray-500 hover:text-primary-500 hover:bg-gray-50 focus:z-10 focus:outline-none focus:border-light-blue-darker focus:shadow-outline active:bg-gray-100 active:text-primary-500 transition ease-in-out duration-150 rounded-l-md"
+            class="relative inline-flex items-center px-2 py-2 border border-default bg-default text-sm leading-tight font-medium text-primary hover:text-primary hover:bg-gray-50 focus:z-10 focus:outline-none focus:border-action focus:shadow-outline active:bg-gray-100 active:text-primary transition ease-in-out duration-150 rounded-l-md"
             >
             <icon value="angle-left" />
         </button>
-        <span class="-ml-px relative inline-flex items-center z-0 px-3 py-2 border border-granite-grey-15 bg-white text-sm leading-tight font-light text-gray-700">
+        <span class="-ml-px relative inline-flex items-center z-0 px-3 py-2 border border-default bg-default text-sm leading-tight font-light text-primary">
             {{ paginator.currentPage }} / <a href="/goto-last-page" class="ml-1 hover:text-primary-500">{{ paginator.lastPage }}</a>
         </span>
         <button 
             rel="next" 
             aria-label="next page" 
-            class="relative inline-flex items-center px-2 py-2 border border-granite-grey-15 bg-white text-sm leading-tight font-medium text-gray-500 hover:text-primary-500 hover:bg-gray-50 focus:z-10 focus:outline-none focus:border-light-blue-darker focus:shadow-outline active:bg-gray-100 active:text-primary-500 transition ease-in-out duration-150 -ml-px rounded-r-md"
+            class="relative inline-flex items-center px-2 py-2 border border-default bg-default text-sm leading-tight font-medium text-primary hover:text-primary hover:bg-gray-50 focus:z-10 focus:outline-none focus:border-action focus:shadow-outline active:bg-gray-100 active:text-primary transition ease-in-out duration-150 -ml-px rounded-r-md"
             >
             <icon value="angle-right" />
         </button>
