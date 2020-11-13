006780405eaefe8d79eabb202a33870ead3dc1af
sebastian thielke <seb@firstnoodle.io>
2020-11-13 11:52:03 +0100

    views/Scope → implementing TextEditor

--- src/views/Audit/views/Scope.vue
+++ src/views/Audit/views/Scope.vue
@@ -1,7 +1,7 @@
 <template>
-    <view-content title="Scope" icon="scope">
+    <view-content title="Scope" icon="scope" fitToViewHeight>
 
-        Content
+        <text-editor expand-vertically :content="editorContent" @change="onEditorChange" />
 
         <template #footer>
             <view-content-footer-link :to="{ name: 'Audit requirements' }" icon="index-finger-up" label="Requirements" placement="left" />
@@ -11,11 +11,22 @@
 </template>
 
 <script>
+import TextEditor from '~/components/TextEditor';
 import ViewContent from '~/components/application/ViewContent';
 import ViewContentFooterLink from '~/components/application/ViewContentFooterLink';
 
 export default {
     name: 'Scope',
-    components: { ViewContent, ViewContentFooterLink }
+    components: { TextEditor, ViewContent, ViewContentFooterLink },
+    data() {
+        return {
+            editorContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque risus vel pulvinar lobortis. Ut quis lobortis quam. Phasellus quis nunc id sem accumsan scelerisque vitae in tellus. Ut vulputate lorem leo, at sodales elit mollis ac. Mauris pharetra sit amet lectus vitae rhoncus. Quisque et dui ac metus consequat laoreet. Ut faucibus non neque eget efficitur. Aenean id odio vulputate, rhoncus magna nec, vehicula leo. In elit est, semper auctor mauris eu, molestie tempor est. Aenean et rhoncus nunc, nec pretium lorem. Nunc quam nisl, convallis vel purus sed, congue imperdiet turpis. Duis mi turpis, commodo non est nec, semper semper nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque risus vel pulvinar lobortis. Ut quis lobortis quam. Phasellus quis nunc id sem accumsan scelerisque vitae in tellus. Ut vulputate lorem leo, at sodales elit mollis ac. Mauris pharetra sit amet lectus vitae rhoncus. Quisque et dui ac metus consequat laoreet. Ut faucibus non neque eget efficitur. Aenean id odio vulputate, rhoncus magna nec, vehicula leo. In elit est, semper auctor mauris eu, molestie tempor est. Aenean et rhoncus nunc, nec pretium lorem. Nunc quam nisl, convallis vel purus sed, congue imperdiet turpis. Duis mi turpis, commodo non est nec, semper semper nibh.'
+        }
+    },
+    methods: {
+        onEditorChange(value) {
+            console.log(value);
+        },
+    }
 }
 </script>
