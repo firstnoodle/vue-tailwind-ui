5a3b58574dbf2011dd63a6fc365ecde4f572a6f8
sebastian thielke <seb@firstnoodle.io>
2020-11-14 00:12:12 +0100

    TextEditor → added slot for header ui, header became footer, softFocus, clickoutside directive, styling

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -1,78 +1,72 @@
 <template>
     <div
-        class="flex flex-col my-4 border border-subtle rounded focus-within:shadow-outline focus-within:border-action"
-        :class="{ 'flex-1 min-h-0' : expandVertically }"
+        v-clickoutside="onClickOutside"
+        class="flex flex-col my-4 border rounded focus-within:shadow-outline focus-within:border-action"
+        :class="{ 'flex-1 min-h-0' : expandVertically, 'shadow-outline border-action' : softFocus, 'border-subtle' : !softFocus }"
     >
+        <editor-content class="flex-1 overflow-y-auto rounded-t" :editor="editor" />
         <!-- MENU -->
         <editor-menu-bar
-            class="flex-0 min-h-0 p-1 bg-default rounded-t overflow-hidden border-b border-subtle focus-within:opacity-100"
+            class="flex-0 min-h-0 p-1 rounded-b overflow-hidden"
+            :class="computedMenuClass"
             :editor="editor"
             v-slot="{ commands, isActive }"
         >
-            <div class="">
-                <text-editor-button @click="commands.undo">
-                    <icon value="arrow-left" />
-                </text-editor-button>
-                <text-editor-button @click="commands.redo">
-                    <icon value="arrow-right" />
-                </text-editor-button>
-                <text-editor-button 
-                    :active="isActive.bold()" 
-                    @click="commands.bold"
-                    >
-                    {{'B'}}
-                </text-editor-button>
-                <text-editor-button 
-                    :active="isActive.italic()" 
-                    @click="commands.italic"
-                    >
-                    {{'I'}}
-                </text-editor-button>
-                <text-editor-button 
-                    :active="isActive.underline()" 
-                    @click="commands.underline"
-                    >
-                    {{'U'}}
-                </text-editor-button>
-                <text-editor-button
-                    :active="isActive.heading({ level: 1 })"
-                    @click="commands.heading({ level: 1 })"
-                    >
-                    {{'H1'}}
-                </text-editor-button>
-                <text-editor-button
-                    :active="isActive.heading({ level: 2 })"
-                    @click="commands.heading({ level: 2 })"
-                    >
-                    {{'H2'}}
-                </text-editor-button>
-                <text-editor-button
-                    :active="isActive.heading({ level: 3 })"
-                    @click="commands.heading({ level: 3 })"
-                    >
-                    {{'H3'}}
-                </text-editor-button>
-                <text-editor-button
-                    :active="isActive.bullet_list()"
-                    @click="commands.bullet_list"
-                    >
-                    <icon value="itenary" />
-                </text-editor-button>
-                <text-editor-button
-                    :active="isActive.ordered_list()"
-                    @click="commands.ordered_list"
-                    >
-                    <icon value="page" />
-                </text-editor-button>
+            <div class="flex items-center justify-between">
+                <div>
+                    <text-editor-button @click="onEditorMenuClick(commands.undo)">
+                        <icon value="arrow-left" />
+                    </text-editor-button>
+                    <text-editor-button @click="onEditorMenuClick(commands.redo)">
+                        <icon value="arrow-right" />
+                    </text-editor-button>
+                    <text-editor-button 
+                        :active="isActive.bold()" 
+                        @click="onEditorMenuClick(commands.bold)"
+                        >
+                        {{'B'}}
+                    </text-editor-button>
+                    <text-editor-button 
+                        :active="isActive.italic()" 
+                        @click="onEditorMenuClick(commands.italic)"
+                        >
+                        {{'I'}}
+                    </text-editor-button>
+                    <text-editor-button 
+                        :active="isActive.underline()" 
+                        @click="onEditorMenuClick(commands.underline)"
+                        >
+                        {{'U'}}
+                    </text-editor-button>
+                    <text-editor-button
+                        :active="isActive.heading({ level: 1 })"
+                        @click="onSpecialCommand(commands.heading({ level: 1 }))"
+                        >
+                        {{'H1'}}
+                    </text-editor-button>
+                    <text-editor-button
+                        :active="isActive.bullet_list()"
+                        @click="onEditorMenuClick(commands.bullet_list)"
+                        >
+                        <icon value="itenary" />
+                    </text-editor-button>
+                    <text-editor-button
+                        :active="isActive.ordered_list()"
+                        @click="onEditorMenuClick(commands.ordered_list)"
+                        >
+                        <icon value="page" />
+                    </text-editor-button>
+                </div>
+                <div v-if="$slots.header">
+                    <slot name="header" />
+                </div>
             </div>
         </editor-menu-bar>
-
-        <!-- CONTENT -->
-        <editor-content class="flex-1 overflow-y-auto rounded-b" :editor="editor" />
     </div>
 </template>
 
 <script>
+import Clickoutside from '~/utils/click-outside';
 import Icon from '~/components/Icon';
 import TextEditorButton from "~/components/TextEditorButton";
 import { Editor, EditorContent, EditorMenuBar } from "tiptap";
@@ -91,6 +85,7 @@ import {
 
 export default {
     name: "TextEditor",
+    directives: { Clickoutside },
     components: {
         EditorContent,
         EditorMenuBar,
@@ -111,8 +106,16 @@ export default {
         return {
             editor: new Editor({
                 content: this.content,
+                onBlur: () => {
+                    this.editorFocussed = false;
+                    this.$emit('blur');
+                },
+                onFocus: () => {
+                    this.editorFocussed = true;
+                    this.softFocus = true;
+                    this.$emit('focus');
+                },
                 onUpdate: ({ getHTML }) => {
-                    // get new content on update
                     this.$emit("change", getHTML());
                 },
                 extensions: [
@@ -121,7 +124,7 @@ export default {
                     new BulletList(),
                     new HardBreak(),
                     new Heading({
-                        levels: [1, 2, 3]
+                        levels: [1]
                     }),
                     new History(),
                     new Italic(),
@@ -130,12 +133,33 @@ export default {
                     new OrderedList(),
                     new Underline()
                 ]
-            })
+            }),
+            editorFocussed: false,
+            softFocus: false,
         };
+    },
+    computed: {
+        computedMenuClass() {
+            return this.softFocus
+                ? 'bg-gray-100 border-t border-default opacity-100'
+                : 'bg-default border-t border-subtle opacity-50';
+        }
     },
     beforeDestroy() {
         // Always destroy your editor instance when it's no longer needed
         this.editor.destroy();
+    },
+    methods: {
+        onClickOutside() {
+            this.softFocus = false;
+        },
+        onEditorMenuClick(command) {
+            command();
+            this.softFocus = true;
+        },
+        onSpecialCommand() {
+            this.softFocus = true;
+        }
     }
 };
 </script>
