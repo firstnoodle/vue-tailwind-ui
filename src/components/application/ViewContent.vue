54f3a3f6a13215407bd2c17981b09143b84977ad
sebastian thielke <seb@firstnoodle.io>
2020-11-13 11:33:38 +0100

    ViewContent → added fitToViewHeight prop

--- src/components/application/ViewContent.vue
+++ src/components/application/ViewContent.vue
@@ -1,17 +1,24 @@
 <template>
     <section class="flex flex-col h-full">
-        <header class="flex items-center py-2 text-brand border-b border-subtle">
+        <header 
+            class="flex items-center py-2 text-brand border-b border-subtle" 
+            :class="{ 'flex-0 min-h-0' : fitToViewHeight }"
+            >
             <icon :value="icon" />
             <span class="ml-2 text-sm">
                 {{ title }}
             </span>
         </header>
-
-        <main class="py-5">
+        <main 
+            class="" 
+            :class="{ 'flex-1 min-h-0 flex flex-col py-5' : fitToViewHeight, 'pt-5 pb-16' : !fitToViewHeight }"
+            >
             <slot />
         </main>
-
-        <footer class="mt-auto flex items-center justify-between py-2 border-t border-subtle">
+        <footer 
+            class="flex items-center justify-between mt-auto py-2 border-t border-subtle" 
+            :class="{ 'flex-0 min-h-0' : fitToViewHeight }"
+            >
             <slot name="footer" />
         </footer>
     </section>
@@ -21,14 +28,18 @@
 export default {
     name: 'ViewContent',
     props: {
-        title: {
-            type: String,
-            required: true
+        fitToViewHeight: {
+            type: Boolean,
+            default: false
         },
         icon: {
             type: String,
             required: true
-        }
+        },
+        title: {
+            type: String,
+            required: true
+        },
     }
 }
 </script>
