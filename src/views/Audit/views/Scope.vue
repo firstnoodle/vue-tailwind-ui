69ac4ac746486a74ac957da25b98799999b6d5ce
sebastian thielke <seb@firstnoodle.io>
2020-11-14 00:15:07 +0100

    views/Scope → Now working as it should
    Save button, status message, beforeRouteLeave, etc.

--- src/views/Audit/views/Scope.vue
+++ src/views/Audit/views/Scope.vue
@@ -1,7 +1,22 @@
 <template>
     <view-content title="Scope" icon="scope" fitToViewHeight>
 
-        <text-editor expand-vertically :content="editorContent" @change="onEditorChange" />
+        <text-editor ref="editor" expand-vertically :content="editorContent" @change="onEditorChange">
+            <template #header>
+                <span class="mr-2 text-xs text-secondary">
+                    {{ computedStatusMessage }}
+                </span>
+                <base-button 
+                    :disabled="!computedDirtyFlag"
+                    :loading="saving"
+                    size="xs"
+                    type="primary" 
+                    @click="save"
+                    >
+                    {{ 'SAVE' }}
+                </base-button>
+            </template>
+        </text-editor>
 
         <template #footer>
             <view-content-footer-link :to="{ name: 'Audit requirements' }" icon="index-finger-up" label="Requirements" placement="left" />
@@ -11,22 +26,67 @@
 </template>
 
 <script>
+import BaseButton from '~/components/BaseButton';
 import TextEditor from '~/components/TextEditor';
 import ViewContent from '~/components/application/ViewContent';
 import ViewContentFooterLink from '~/components/application/ViewContentFooterLink';
 
 export default {
     name: 'Scope',
-    components: { TextEditor, ViewContent, ViewContentFooterLink },
+    components: { BaseButton, TextEditor, ViewContent, ViewContentFooterLink },
     data() {
         return {
-            editorContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque risus vel pulvinar lobortis. Ut quis lobortis quam. Phasellus quis nunc id sem accumsan scelerisque vitae in tellus. Ut vulputate lorem leo, at sodales elit mollis ac. Mauris pharetra sit amet lectus vitae rhoncus. Quisque et dui ac metus consequat laoreet. Ut faucibus non neque eget efficitur. Aenean id odio vulputate, rhoncus magna nec, vehicula leo. In elit est, semper auctor mauris eu, molestie tempor est. Aenean et rhoncus nunc, nec pretium lorem. Nunc quam nisl, convallis vel purus sed, congue imperdiet turpis. Duis mi turpis, commodo non est nec, semper semper nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque risus vel pulvinar lobortis. Ut quis lobortis quam. Phasellus quis nunc id sem accumsan scelerisque vitae in tellus. Ut vulputate lorem leo, at sodales elit mollis ac. Mauris pharetra sit amet lectus vitae rhoncus. Quisque et dui ac metus consequat laoreet. Ut faucibus non neque eget efficitur. Aenean id odio vulputate, rhoncus magna nec, vehicula leo. In elit est, semper auctor mauris eu, molestie tempor est. Aenean et rhoncus nunc, nec pretium lorem. Nunc quam nisl, convallis vel purus sed, congue imperdiet turpis. Duis mi turpis, commodo non est nec, semper semper nibh.'
+            audit_id: null,
+            editorContent: null,
+            savedContent: null,
+            saving: false,
+            saved: false
+        }
+    },
+    computed: {
+        computedDirtyFlag() {
+            return this.editorContent !== this.savedContent;
+        },
+        computedStatusMessage() {
+            if(this.saving) return '';
+            if(this.computedDirtyFlag) return 'You have unsaved changes';
+            if(!this.computedDirtyFlag && this.saved) return 'Changes was saved!'
+            return '';
+        }
+    },
+    created() {
+        this.audit_id = this.$route.params.id;
+        this.editorContent = this.$store.state.audits[this.audit_id].scope;
+    },
+    mounted() {
+        this.savedContent = this.editorContent = this.$refs.editor.editor.getHTML();
+    },
+    beforeRouteLeave (to, from, next) {
+        if(this.computedDirtyFlag) {
+            const answer = window.confirm('You have unsaved changes. Are you sure you want to leave?');
+            if (answer) {
+                next();
+            } else {
+                next(false);
+            }
+        } else {
+            next();
         }
     },
     methods: {
         onEditorChange(value) {
             console.log(value);
+            this.editorContent = value;
         },
+        save() {
+            this.saving = true;
+            setTimeout(() => {
+                this.$store.commit(`audits/${this.audit_id}/UPDATE_SCOPE`, this.editorContent);
+                this.savedContent = this.editorContent;
+                this.saving = false;
+                this.saved = true;
+            }, 1000);
+        }
     }
 }
 </script>
