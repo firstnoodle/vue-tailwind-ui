b9ef2dc65f6352d1088ab29eab1a4e91d22b5a97
sebastian thielke <seb@firstnoodle.io>
2020-11-16 22:01:49 +0100

    Suggestions → implemented the whole thing

--- src/views/Audit/views/Suggestions.vue
+++ src/views/Audit/views/Suggestions.vue
@@ -1,7 +1,60 @@
 <template>
     <view-content title="Suggestions" icon="message">
 
-        Content
+        <draggable 
+            v-model="suggestions" 
+            v-bind="dragOptions"
+            handle=".drag-handle"
+            >
+            <transition-group type="transition">
+                <list-item 
+                    v-for="item in suggestions"
+                    :key="item.listId"
+                    :draggable="true" 
+                    :edit="item.edit"
+                    editable
+                    @edit="onItemEdit(item)"
+                    @delete="$store.commit(`audits/${audit_id}/suggestions/DELETE_ITEM`, item.id)"
+                    class="last:mb-4"
+                    >
+                    <div class="css-rich-text w-full" v-html="item.description"></div>
+
+                    <template #edit>
+                        <div class="flex pr-0 md:pr-16 mb-2">
+                            <div class="w-full">
+                                <text-editor inline ref="editor" :content="editorContent" @change="onEditorChange" />
+                            </div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <base-button 
+                                ref="saveButton"
+                                icon="plus"
+                                type="primary"
+                                :disabled="computedSaveButtonDisabled"
+                                :loading="posting" 
+                                @click.stop.prevent="saveItem" 
+                            >
+                                {{ 'Add suggestion' }}
+                            </base-button>
+                            <base-button @click="cancelEditSuggestion(item)" plain type="primary">Cancel</base-button>
+                        </div>
+                    </template>
+                </list-item>
+            </transition-group>
+        </draggable>
+
+        <!-- Add new button -->
+        <div class="" v-if="showAddNewButton">
+            <base-button 
+                icon="plus"
+                plain 
+                ref="addNewButton"
+                type="primary" 
+                @click.stop.prevent="onOpenNewItem" 
+                >
+                Add suggestion
+            </base-button>
+        </div>
 
         <template #footer>
             <view-content-footer-link :to="{ name: 'Audit findings' }" icon="index-finger-right" label="Findings" placement="left" />
@@ -11,11 +64,102 @@
 </template>
 
 <script>
+import BaseButton from '~/components/BaseButton.js';
+import draggable from 'vuedraggable';
+import ListItem from '~/components/ListItem';
+import TextEditor from '~/components/TextEditor';
+
 import ViewContent from '~/components/application/ViewContent';
 import ViewContentFooterLink from '~/components/application/ViewContentFooterLink';
 
 export default {
     name: 'Suggestions',
-    components: { ViewContent, ViewContentFooterLink }
+    components: { BaseButton, draggable, ListItem, TextEditor, ViewContent, ViewContentFooterLink },
+    data() {
+        return {
+            audit_id: null,
+            dragOptions: {
+                animation: 200,
+                group: "description",
+                disabled: false,
+                ghostClass: "drag-ghost"
+            },
+            editorContent: '',
+            posting: false,
+            showAddNewButton: true,
+        }
+    },
+    computed: {
+        computedSaveButtonDisabled() {
+            if(!this.editorContent.length) return true;
+            return false;
+        },
+        suggestions: {
+            get() {
+                return this.$store.state.audits[this.audit_id].suggestions.items;
+            },
+            set(value) {
+                this.$store.commit(`audits/${this.audit_id}/suggestions/UPDATE_ITEMS`, value)
+            }
+        }
+    },
+
+    created() {
+        this.audit_id = this.$route.params.id; 
+    },
+
+    beforeDestroy() {
+        this.$store.dispatch(`audits/${this.audit_id}/suggestions/cancelEditItems`);
+    },
+
+    methods: {
+        cancelEditSuggestion(item) {
+            if(!item) return;
+
+            this.editorContent = '';
+            this.$store.dispatch(`audits/${this.audit_id}/suggestions/cancelEditItem`, item);
+
+            // TODO: this prevents an unwanted ui glitch - $nextTick doesn't do the job...
+            setTimeout(() => this.showAddNewButton = true, 20);
+        },
+
+        onEditorChange(value) {
+            this.editorContent = value;
+        },
+
+        onItemEdit(item) {
+            this.$store.dispatch(`audits/${this.audit_id}/suggestions/cancelEditItems`);
+            this.showAddNewButton = false;
+            item.edit = true
+            this.editorContent = item.description;
+        },
+
+        onOpenNewItem() {
+            this.$store.commit(
+                `audits/${this.audit_id}/suggestions/ADD_ITEM`,
+                {
+                    description: null
+                }
+            );
+            this.showAddNewButton = false;
+            this.$nextTick(() => this.$refs.editor[0].focus());
+        },
+
+        saveItem() {                
+            this.posting = true;
+            console.log(this.editorContent);
+            setTimeout(() => {
+                this.$store.commit(`audits/${this.audit_id}/suggestions/SAVE_ITEM`, { id: Date.now(), description: this.editorContent });
+                this.editorContent = '';
+                this.posting = false;
+
+                this.showAddNewButton = true;
+
+                this.$nextTick(() => {
+                    this.$refs.addNewButton.$el.focus();
+                });
+            }, 1000);
+        },
+    }
 }
 </script>
