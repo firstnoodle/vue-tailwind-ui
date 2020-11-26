9c6675656523d7fb3aeb6769c0c0847f3e8be290
sebastian thielke <seb@firstnoodle.io>
2020-11-26 13:36:57 +0100

    findings module → added crud actions+mutations for handling finding-references

--- src/store/modules/findings.js
+++ src/store/modules/findings.js
@@ -1,9 +1,31 @@
+import Vue from 'vue';
 import crud from './crud.js';
+import { setNestedProp } from '~/utils/vue';
 
 export default {
     namespaced: true,
     actions: {
         ...crud.actions,
+        /**
+         * The below actions are almost identical copies of cancelEditItem and cancelEditItems in crud.js
+         * but are needed to handle the reference items in the nested references array
+         */ 
+        cancelEditFindingReference({state, commit}, { finding_id, reference_id }) {
+            const finding = state.items.find(item => item.id === finding_id);
+            const reference = finding.data.references.find(item => item.id === reference_id);
+
+            if(!reference.id) {
+                commit('DELETE_FINDING_REFERENCE', { finding, reference });
+            } else if(reference.uiState.edit) {
+                commit('CANCEL_EDIT_FINDING_REFERENCE', reference);
+            }
+        },
+        cancelEditFindingReferences({state, dispatch}, finding_id) {
+            const finding = state.items.find(item => item.id === finding_id);
+            for(const reference of finding.data.references) {
+                dispatch('cancelEditFindingReference', finding.id, reference.id);
+            }
+        },
     },
     getters: {
         ...crud.getters,
@@ -25,5 +47,65 @@ export default {
     },
     mutations: {
         ...crud.mutations,
+        /**
+         * The below mutations are almost identical copies of the mutations in crud.js
+         * but are needed to handle the reference items in the nested references array
+         */ 
+        ADD_FINDING_REFERENCE(state, {finding_id, reference}) {
+            const finding = state.items.find(item => item.id === finding_id);
+            if(!finding) {
+                console.error(`[ ADD_FINDING_REFERENCE ] no id passed..`);
+                return;
+            }
+            finding.data.references.push({
+                id: null,
+                data: reference,
+                uiState: {
+                    edit: true,
+                    listId: Date.now(),
+                    selected: false
+                }
+            });
+        },
+        CANCEL_EDIT_FINDING_REFERENCE(state, reference) {
+            setNestedProp(
+                reference,
+                'uiState.edit', 
+                false
+            );
+        },
+        DELETE_FINDING_REFERENCE(state, {finding, reference}) {
+            Vue.delete(
+                finding.data.references, 
+                finding.data.references.findIndex(item => item.id === reference.id)
+            );
+        },
+        SAVE_FINDING_REFERENCE(state, { finding_id, reference }) {
+            const finding = state.items.find(item => item.id === finding_id);
+            const target = finding.data.references.find(item => item.uiState.edit);
+            // console.log('test', reference, finding, target);
+            // console.log(finding.data.references.findIndex(item => item.uiState.edit));
+            Vue.set(
+                finding.data.references,
+                finding.data.references.findIndex(item => item.uiState.edit),
+                {
+                    data: reference,
+                    id: Date.now(),
+                    uiState: {
+                        edit: false,
+                        listId: target.uiState.listId,
+                        selected: false,
+                    }
+                }
+            );
+        },
+        UPDATE_FINDING_REFERENCES(state, {finding_id, value}) {
+            const finding = state.items.find(item => item.id === finding_id);
+            if(!finding) {
+                console.error(`[ UPDATE_FINDING_REFERENCES ] no finding was found`);
+                return;
+            }
+            finding.data.references = value;
+        }
     }
 }
