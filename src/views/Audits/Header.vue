df4c1e86915b9bf4fda60fa579a6b071f85594bd
sebastian thielke <seb@firstnoodle.io>
2020-11-26 14:25:41 +0100

    views/Audits/Header → emits `new-audit` + removed createAudit method

--- src/views/Audits/Header.vue
+++ src/views/Audits/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <view-header>
         <template #sidebar>
-            <base-button plain type="primary" icon="plus" class="w-full inline-flex" @click="createAudit">NEW AUDIT</base-button>
+            <base-button plain type="primary" icon="plus" class="w-full inline-flex" @click="$emit('new-audit')">NEW AUDIT</base-button>
         </template>
         <template #main>
             <div class="flex-1 flex items-center justify-between max-w-4xl m-auto">
@@ -14,7 +14,6 @@
 </template>
 
 <script>
-import auditModule from '~/store/modules/audit.js';
 import BaseButton from '~/components/BaseButton';
 import Pagination from '~/components/Pagination';
 import ViewHeader from '~/components/application/ViewHeader';
@@ -22,12 +21,5 @@ import ViewHeader from '~/components/application/ViewHeader';
 export default {
     name: 'Header',
     components: { BaseButton, Pagination, ViewHeader },
-    methods: {
-        createAudit() {
-            const id = Date.now();
-            this.$store.registerModule(['audits', id.toString()], auditModule);
-            this.$router.push({ name: 'Audit details',  params: { id }});
-        }
-    }
 }
 </script>
