447560d34e459248df9827e03cfe29b955cd83db
sebastian thielke <seb@firstnoodle.io>
2020-11-27 21:02:49 +0100

    audit module → UPDATE_RATING mutation + state rating prop

--- src/store/modules/audit.js
+++ src/store/modules/audit.js
@@ -4,11 +4,15 @@ import findings from '~/store/modules/findings.js';
 export default {
     namespaced: true,
     mutations: {
+        UPDATE_RATING(state, value) {
+            state.rating = value;
+        },
         UPDATE_SCOPE(state, value) {
             state.scope = value;
         }
     },
     state: {
+        rating: null,
         scope: '<p>The audit covers the Quality Management System and ISO/GMP processes applicable for:</p><p><ul><li>Dept. name, dept. no</li></ul></p>',
     },
     modules: { 
