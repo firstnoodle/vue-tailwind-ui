9b1ca1c93c60e0813e7c2c147eae8db4108b0acc
sebastian thielke <seb@firstnoodle.io>
2020-11-27 20:55:52 +0100

    Created constants file

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+
+export const AUDIT_RATINGS = {
+    NOT_MEETING_STANDARDS: "Not meeting standards",
+    LOW: "Low",
+    SATISFACTORY: "Satisfactory",
+    HIGH: "High",
+    OUTSTANDING: "Outstanding"
+};
+
+export const SEVERITY_LEVELS = {
+    MINOR: "Minor",
+    MAJOR: "Major",
+    CRITICAL: "Critical",
+};
