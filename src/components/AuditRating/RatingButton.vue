de06ce79f153b03bb5f671f9374417e2b162100e
sebastian thielke <seb@firstnoodle.io>
2020-11-27 21:02:14 +0100

    RatingButton → focus:z-10

--- src/components/AuditRating/RatingButton.vue
+++ src/components/AuditRating/RatingButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button 
-        class="flex items-center justify-center h-10 w-1/5 border-r text-white border-white bg-gray-200 last:border-none focus:outline-none focus:bg-action"
+        class="z-0 flex items-center justify-center h-10 w-1/5 border-r text-white border-white bg-gray-200 last:border-none focus:outline-none focus:shadow-outline focus:z-10"
         :class="{ 'bg-action' : active }"
         @click="$emit('click')"
         >
