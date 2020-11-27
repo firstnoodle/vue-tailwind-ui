940a0648779334c5cd3e485b7553b5c325f02809
sebastian thielke <seb@firstnoodle.io>
2020-11-27 21:01:50 +0100

    AuditRating/main → showing finding(severity) count and suggestions count

--- src/components/AuditRating/main.vue
+++ src/components/AuditRating/main.vue
@@ -18,7 +18,7 @@
                 <rating-button v-for="option in auditRatingOptions" :key="option" :active="option === rating" @click="$emit('rating-changed', option)" />
             </div>
         </div>
-        <div class="flex-0 inline-block">
+        <div class="flex-0 inline-block border-r-2 border-white">
             <div class="flex items-center justify-center w-full h-20 bg-gray-200 text-gray-600 text-center text-sm font-bold border-b-2 border-white">
                 Number and<br>severity<br>of findings
             </div>
@@ -26,9 +26,26 @@
                 <rating-label v-for="option in severityLevelOptions" :key="option">{{ option }}</rating-label>
             </div>
             <div class="w-full flex border-t-2 border-white">
-                <div class="flex items-center justify-center h-10 w-1/3 bg-red-700 text-sm text-white border-r border-white">1</div>
-                <div class="flex items-center justify-center h-10 w-1/3 bg-yellow-500 text-sm text-secondary border-r border-white">4</div>
-                <div class="flex items-center justify-center h-10 w-1/3 bg-blue-200 text-sm text-secondary border-none">2</div>
+                <div class="flex items-center justify-center h-10 w-1/3 bg-blue-200 text-sm text-secondary border-r border-white">
+                    {{ $store.getters[`audits/${audit_id}/findings/numberOfMinorFindings`] }}
+                </div>
+                <div class="flex items-center justify-center h-10 w-1/3 bg-yellow-500 text-sm text-secondary border-r border-white">
+                    {{ $store.getters[`audits/${audit_id}/findings/numberOfMajorFindings`] }}
+                </div>
+                <div class="flex items-center justify-center h-10 w-1/3 bg-red-700 text-sm text-white border-none">
+                    {{ $store.getters[`audits/${audit_id}/findings/numberOfCriticalFindings`] }}
+                </div>
+            </div>
+        </div>
+        <div class="flex-0 inline-block">
+            <div class="flex items-center justify-center w-full h-20 bg-gray-200 text-gray-600 text-center text-sm font-bold border-b-2 border-white"></div>
+            <div class="flex">
+                <rating-label>Suggestions</rating-label>
+            </div>
+            <div class="w-full flex border-t-2 border-white">
+                <div class="flex items-center justify-center h-10 w-full bg-gray-200 text-sm text-secondary border-none">
+                    {{ $store.state.audits[audit_id].suggestions.items.length }}
+                </div>
             </div>
         </div>
     </div>
