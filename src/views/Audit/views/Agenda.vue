ba8b990eacf67ac55a5df63e0af7950e3a223b09
sebastian thielke <seb@firstnoodle.io>
2020-12-01 15:07:20 +0100

    views/Agenda → cleanup

--- src/views/Audit/views/Agenda.vue
+++ src/views/Audit/views/Agenda.vue
@@ -2,12 +2,12 @@
     <view-content title="Agenda" icon="clock">
 
         <date-picker 
-            type="datetime"
+            ref="datepicker"
+            type="date"
             :value="date" 
             :options="options" 
             @change="onDateChange" 
             />
-
 
         <template #footer>
             <view-content-footer-link :to="{ name: 'Audit focus areas' }" icon="crosshair" label="Focus Areas" placement="left" />
@@ -17,23 +17,11 @@
 </template>
 
 <script>
-// import { arrowKeys } from "./utils/keyboard.js";
-import { DATE/* , DATE_TIME */ } from "~/utils/input-formatting.js";
-import { disableWeekday, WEEKDAYS } from "~/utils/time/calendar.js";
-import { loopRange } from "~/utils/array.js";
+import { DATE } from "~/utils/input-formatting.js";
 import DatePicker from '~/components/DatePicker/main';
 
 import ViewContent from '~/components/application/ViewContent';
 import ViewContentFooterLink from '~/components/application/ViewContentFooterLink';
-
-/**
- * DatePicker
- *   main
- *   
- * Calendar
- *   main (renderless)
- *   calendar
- */
 
 export default {
     name: 'Agenda',
@@ -41,31 +29,15 @@ export default {
     data() {
         return {
             audit_id: this.$route.params.audit,
-            date: "2020-11-18",
-            inputContent: '2020',
-            inputFormat: DATE,
-            inputValid: false,
+            date: null,
             options: {
                 format: DATE,
-                disabledDate: disableWeekday("sunday"),
                 weekStart: 1,
             },
-            weekdays: null,
         }
-    },
-    created() {
-        this.weekdays = WEEKDAYS.map((day, index) => {
-            return WEEKDAYS[
-                loopRange(
-                    index + this.options.weekStart,
-                    WEEKDAYS.length
-                )
-            ];
-        })
     },
     methods: {
         onDateChange(date) {
-            console.log(date);
             this.date = date;
         },
     }
