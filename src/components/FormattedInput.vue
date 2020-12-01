d4d22970e91fb7f8333ba2dda5eec0acd1086a29
sebastian thielke <seb@firstnoodle.io>
2020-12-01 15:05:38 +0100

    FormattedInput → indent all + value not required

--- src/components/FormattedInput.vue
+++ src/components/FormattedInput.vue
@@ -5,9 +5,10 @@
         :content="value"
         placeholder="2019-01-01 00:00"
         @deleteSelection="onDeleteSelection"
+        @focus="$emit('focus')"
         @keyEvent="onInputKeyEvent"
         @paste="onInputPaste"
-    />
+        />
 </template>
 
 <script>
@@ -15,70 +16,71 @@ import { inputValid, processInputValue } from "~/utils/input-formatting.js";
 import ControlledInput from "~/components/ControlledInput.vue";
 
 export default {
-  name: "FormattedInput",
-  components: {
-    ControlledInput,
-  },
-  props: {
-    value: {
-        type: String,
-        required: true,
+    name: "FormattedInput",
+    components: { ControlledInput },
+    props: {
+        value: {
+            type: String,
+        },
+        format: {
+            type: Object,
+            required: true,
+        },
     },
-    format: {
-        type: Object,
-        required: true
-    }
-  },
-  data() {
-    return {
-      paste: null,
-    };
-  },
-  mounted() {
-    if (!inputValid(this.value, this.format)) {
-      this.$emit("change", "");
-    }
-  },
-  methods: {
-    deleteLastChar() {
-      this.$emit("change", this.value.slice(0, this.value.length - 1));
+    data() {
+        return {
+            paste: null,
+        };
     },
-
-    onDeleteSelection(selection) {
-      this.$emit("change", this.value.replace(selection, ""));
-    },
-
-    onInputKeyEvent(event) {
-      if (this.paste) {
-        if (inputValid(this.paste.pastedValue, this.format)) {
-          this.$emit("change", this.paste.pastedValue);
-        } else {
-          this.$emit("change", this.paste.oldValue);
+    mounted() {
+        if (!inputValid(this.value, this.format)) {
+            this.$emit("change", "");
         }
-        this.paste = null;
-      } else {
-        if (event.type === "keydown") {
-          if (event.keyCode === 8) {
-            this.deleteLastChar();
-          } else if (event.keyCode === 13) {
-            //this.$refs.input.$refs.input.blur()();
-          } else {
-            this.$emit(
-              "change",
-              processInputValue(this.value, this.format, event)
-            );
-          }
-        } else if (event.type === "keyup") {
-          if (event.keyCode === 13) {
-            this.$refs.input.$refs.input.blur();
-          }
-        }
-      }
     },
+    methods: {
+        deleteLastChar() {
+            this.$emit("change", this.value.slice(0, this.value.length - 1));
+        },
 
-    onInputPaste(pastedValue, oldValue) {
-      this.paste = { pastedValue, oldValue };
+        focus() {
+            this.$refs.input.$refs.input.focus();
+        },
+
+        onDeleteSelection(selection) {
+            this.$emit("change", this.value.replace(selection, ""));
+        },
+
+        onInputKeyEvent(event) {
+            if (this.paste) {
+                if (inputValid(this.paste.pastedValue, this.format)) {
+                    this.$emit("change", this.paste.pastedValue);
+                } else {
+                    this.$emit("change", this.paste.oldValue);
+                }
+                this.paste = null;
+            } else {
+                if (event.type === "keydown") {
+                    if (event.keyCode === 8) {
+                        this.deleteLastChar();
+                    } else if (event.keyCode === 13) {
+                        //this.$refs.input.$refs.input.blur()();
+                    } else {
+                        this.$emit(
+                            "change",
+                            processInputValue(this.value, this.format, event)
+                        );
+                    }
+                } else if (event.type === "keyup") {
+                    if (event.keyCode === 13) {
+                        this.$refs.input.$refs.input.blur();
+                    }
+                }
+            }
+        },
+
+        onInputPaste(pastedValue, oldValue) {
+            this.paste = { pastedValue, oldValue };
+        },
     },
-  }
 };
 </script>
