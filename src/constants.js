3cf706c9ca9f178054471d4dcada8b07ea645bbc
sebastian thielke <seb@firstnoodle.io>
2020-12-18 21:57:03 +0100

    constants → added INFORMATION_REQUEST_CRITERIAS

--- src/constants.js
+++ src/constants.js
@@ -7,6 +7,29 @@ export const AUDIT_RATINGS = {
     OUTSTANDING: "Outstanding"
 };
 
+export const INFORMATION_REQUEST_CRITERIAS = [
+    {
+        criteria: 'The following items should be available AFTER the opening meeting',
+        requiresDate: false
+    },
+    {
+        criteria: 'The following items should be available AT the opening meeting',
+        requiresDate: false
+    },
+    {
+        criteria: 'The following items should be available at the beginning of the audit of each department',
+        requiresDate: false
+    },
+    {
+        criteria: 'The following items should be delivered before DATE',
+        requiresDate: true
+    },
+    {
+        criteria: 'The following items are requested for handout to the auditors on DATE',
+        requiresDate: true
+    }
+];
+
 export const ROLES = {
     LEAD_AUDITOR: 'Lead auditor',
     AUDITOR: 'Auditor',
