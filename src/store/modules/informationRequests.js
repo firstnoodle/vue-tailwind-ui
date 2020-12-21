9ec8319d28f9ada921814892f0cb9d2779134868
sebastian thielke <seb@firstnoodle.io>
2020-12-21 22:58:13 +0100

    InformationRequests → CRUD functionality fro periods

--- src/store/modules/informationRequests.js
+++ src/store/modules/informationRequests.js
@@ -13,8 +13,29 @@ export default {
                 commit('CANCEL_EDIT_CRITERIA', criteria);
             }
         },
+        cancelEditCriteriaPeriod({commit}, {criteria, period}) {
+            if(!period.id) {
+                commit('DELETE_CRITERIA_PERIOD', {criteria, period});
+            } else if(period.uiState.edit) {
+                commit('CANCEL_EDIT_CRITERIA_PERIOD', period);
+            }
+        },
     },
     getters: {
+        /**
+         * Check if user has activities on the given date that are being edited
+         * This is to hide the addActivity button for a user on a given date
+         */
+        showCriteriaPeriodButton: () => (criteria, period) => {
+            for(const p of criteria.data.periods) {
+                if(p.data.start_date === period.data.start_date && 
+                    p.data.end_date === period.data.end_date && 
+                    p.uiState.edit === true) {
+                    return false;
+                }
+            }
+            return true;
+        }
     },
     mutations: {
         ADD_CRITERIA(state) {
@@ -32,9 +53,32 @@ export default {
                 }
             })
         },
+        ADD_CRITERIA_PERIOD(state, criteria) {
+            const index = state.criterias.findIndex(c => c.id === criteria.id);
+            state.criterias[index].data.periods.push({
+                id: null,
+                data: {
+                    start_date: null,
+                    end_date: null,
+                    informmation_requests: []
+                },
+                uiState: {
+                    edit: true,
+                    listId: Date.now(),
+                    selected: false
+                }
+            })
+        },
         CANCEL_EDIT_CRITERIA(state, criteria) {
             setNestedProp(
                 criteria,
+                'uiState.edit', 
+                false
+            );
+        },
+        CANCEL_EDIT_CRITERIA_PERIOD(state, period) {
+            setNestedProp(
+                period,
                 'uiState.edit', 
                 false
             );
@@ -43,6 +87,13 @@ export default {
             Vue.delete(
                 state.criterias,
                 state.criterias.findIndex(c => c.id === criteria.id)
+            );
+        },
+        DELETE_CRITERIA_PERIOD(state, {criteria, period}) {
+            const criteriaIndex = state.criterias.findIndex(c => c.id === criteria.id);
+            Vue.delete(
+                state.criterias[criteriaIndex].data.periods,
+                state.criterias[criteriaIndex].data.periods.findIndex(p => p.id === period.id)
             );
         },
         SAVE_CRITERIA(state, criteria) {
@@ -55,7 +106,28 @@ export default {
                     data: {
                         criteria: criteria.description,
                         date: criteria.date,
-                        periods: []
+                        periods: target.data.periods
+                    },
+                    uiState: {
+                        edit: false,
+                        listId: target.uiState.listId,
+                        selected: false
+                    }
+                }
+            )
+        },
+        SAVE_CRITERIA_PERIOD(state, {criteria, period}) {
+            const criteriaIndex = state.criterias.findIndex(c => c.id === criteria.id);
+            const target = state.criterias[criteriaIndex].data.periods.find(p => p.uiState.edit);
+            Vue.set(
+                state.criterias[criteriaIndex].data.periods,
+                state.criterias[criteriaIndex].data.periods.findIndex(p => p.uiState.edit),
+                {
+                    id: Date.now(),
+                    data: {
+                        start_date: period.start_date,
+                        end_date: period.end_date,
+                        informmation_requests: []
                     },
                     uiState: {
                         edit: false,
--- src/views/Audit/views/InformationRequests.vue
+++ src/views/Audit/views/InformationRequests.vue
@@ -1,8 +1,9 @@
 <template>
     <view-content title="Information Requests" icon="information">
 
+        <!-- Criterias -->
         <div 
-            class=""
+            class="pb-4 mb-8 border-b border-subtle"
             v-for="criteria in $store.state.audits[audit_id].informationRequests.criterias" 
             :key="criteria.uiState.listId"
             >
@@ -13,18 +14,18 @@
                 @edit="onCriteriaEdit(criteria)"
                 @delete="onCriteriaDelete(criteria)"
                 >
-                <div class="flex items-center space-x-2">
-                    <icon value="page" />
-                    <span class="font-bold">
-                        {{ criteria.data.criteria }}
-                    </span>
-                    <span v-if="criteria.data.date" class="px-2 py-1 rounded-md bg-gray-200 text-primary">
-                        {{ criteria.data.date }}
-                    </span>
-                </div>
-
+                <template #default>
+                    <div class="flex items-center space-x-2">
+                        <icon value="page" />
+                        <span class="font-bold">
+                            {{ criteria.data.criteria }}
+                        </span>
+                        <span v-if="criteria.data.date" class="px-2 py-1 rounded-md bg-gray-200 text-primary font-bold">
+                            {{ criteria.data.date }}
+                        </span>
+                    </div>
+                </template>
                 <template #edit>
-                    <!-- Criteria -->
                     <div class="flex pr-0 md:pr-16 mb-2 space-x-2">
                         <div class="w-2/3">
                             <fn-select 
@@ -45,7 +46,7 @@
                         </div>
                         <div class="w-13" v-if="computedCriteriaDatePickerVisible">
                             <date-picker 
-                                ref="datepicker"
+                                ref="criteriaDatePicker"
                                 type="date"
                                 :value="selectedCriteriaDate" 
                                 :options="datePickerOptions" 
@@ -53,11 +54,9 @@
                                 />
                         </div>
                     </div>
-                    <!-- Date* -->
-
                     <div class="flex items-center space-x-2">
                         <base-button 
-                            ref="saveButton"
+                            ref="saveCriteriaButton"
                             icon="plus"
                             type="primary"
                             :disabled="computedSaveCriteriaButtonDisabled"
@@ -70,19 +69,84 @@
                     </div>
                 </template>
             </list-item>
+
+            <!-- Periods -->
+            <div
+                v-for="period in criteria.data.periods"
+                :key="period.uiState.listId"
+                class="pl-4 md:pl-8 py-4 "
+                >
+                <list-item 
+                    deletable
+                    editable
+                    :edit="period.uiState.edit"
+                    @edit="onCriteriaPeriodEdit(period)"
+                    @delete="onCriteriaPeriodDelete(criteria, period)"
+                    >
+                    <template #default>
+                        <div class="flex items-center space-x-2">
+                            <icon value="calendar" />
+                            <span class="font-bold">{{ period.data.start_date }}</span>
+                            <icon value="arrow-right" />
+                            <span class="font-bold">{{ period.data.end_date }}</span>
+                        </div>
+                    </template>
+                    <template #edit>
+                        <div class="flex pr-0 md:pr-16 mb-2 space-x-2">
+                            <date-picker 
+                                ref="periodStartDatePicker"
+                                type="date"
+                                :value="selectedPeriodStartDate" 
+                                :options="datePickerOptions" 
+                                @change="onPeriodStartDateChange" 
+                                />
+                            <date-picker 
+                                ref="periodEndDatePicker"
+                                type="date"
+                                :value="selectedPeriodEndDate" 
+                                :options="datePickerOptions" 
+                                @change="onPeriodEndDateChange" 
+                                />
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <base-button 
+                                ref="savePeriodButton"
+                                icon="plus"
+                                type="primary"
+                                :disabled="computedSavePeriodButtonDisabled"
+                                :loading="savingPeriod" 
+                                @click.stop.prevent="onSaveCriteriaPeriod(criteria)" 
+                            >
+                                {{ period.id ? 'Update period' : 'Add period' }}
+                            </base-button>
+                            <base-button @click="onCancelEditCriteriaPeriod(criteria, period)" plain type="primary">Cancel</base-button>
+                        </div>
+                    </template>
+                </list-item>
+            </div>
+            <div class="py-3 pl-4 md:pl-8">
+                <base-button 
+                    icon="plus"
+                    plain 
+                    ref="addNewPeriodButton"
+                    type="primary" 
+                    @click.stop.prevent="onAddNewCriteriaPeriod(criteria)"
+                    >
+                    Add period
+                </base-button>
+            </div>
         </div>
 
-        <div class="" v-if="showAddNewCriteriaButton">
-            <base-button 
-                icon="plus"
-                plain 
-                ref="addNewCriteriaButton"
-                type="primary" 
-                @click.stop.prevent="onAddNewCriteria" 
-                >
-                Add criteria
-            </base-button>
-        </div>
+        <base-button 
+            v-if="showAddNewCriteriaButton"
+            icon="plus"
+            plain 
+            ref="addNewCriteriaButton"
+            type="primary" 
+            @click.stop.prevent="onAddNewCriteria" 
+            >
+            Add criteria
+        </base-button>
 
         <template #footer>
             <view-content-footer-link :to="{ name: 'Audit agenda' }" icon="clock" label="Agenda" placement="left" />
@@ -114,10 +178,14 @@ export default {
                 weekStart: 1,
             },
             savingCriteria: false,
+            savingPeriod: false,
             selectedCriteria: null,
             selectedCriteriaDate: null,
+            selectedPeriodStartDate: null,
+            selectedPeriodEndDate: null,
             selectedCriteriaOption: null,
-            showAddNewCriteriaButton: true
+            showAddNewCriteriaButton: true,
+            showAddNewPeriodButton: true
         }
     },
     computed: {
@@ -132,6 +200,13 @@ export default {
                     }
                     return true;
                 }
+                return false;
+            }
+            return true;
+        },
+        computedSavePeriodButtonDisabled() {
+            // TODO: check that dates are chronological
+            if(this.selectedPeriodStartDate && this.selectedPeriodEndDate) {
                 return false;
             }
             return true;
@@ -151,8 +226,18 @@ export default {
                 `audits/${this.audit_id}/informationRequests/ADD_CRITERIA`
             );
             this.showAddNewCriteriaButton = false;
-            // this.$nextTick(() => this.$refs.datepicker[0].focus());
+            this.$nextTick(() => this.$refs.criteriaSelect[0].focus());
         },
+
+        onAddNewCriteriaPeriod(criteria) {
+            this.$store.commit(
+                `audits/${this.audit_id}/informationRequests/ADD_CRITERIA_PERIOD`,
+                criteria
+            );
+            this.showAddNewPeriodButton = false;
+            // this.$nextTick(() => this.$refs.periodStartDatePciker[0].focus());
+        },
+
         onCancelEditCriteria(criteria) {
             if(!criteria) return;
 
@@ -164,9 +249,21 @@ export default {
             setTimeout(() => this.showAddNewCriteriaButton = true, 20);
         },
 
+        onCancelEditCriteriaPeriod(criteria, period) {
+            if(!period) return;
+
+            this.selectedPeriodStartDate = null;
+            this.selectedPeriodEndDate = null;
+            this.$store.dispatch(`audits/${this.audit_id}/informationRequests/cancelEditCriteriaPeriod`, {criteria, period});
+
+            // TODO: this prevents an unwanted ui glitch - $nextTick doesn't do the job...
+            setTimeout(() => this.showAddNewPeriodButton = true, 20);
+        },
+
         onCriteriaDelete(criteria) {
             this.$store.commit(`audits/${this.audit_id}/informationRequests/DELETE_CRITERIA`, criteria);
         },
+
         onCriteriaEdit(criteria) {
             this.selectedCriteriaOption = this.criteriaOptions.find(c => {
                 return c.label === criteria.data.criteria;
@@ -177,9 +274,32 @@ export default {
             this.showAddNewCriteriaButton = false;
             criteria.uiState.edit = true;
         },
+
+        onCriteriaPeriodDelete(criteria, period) {
+            this.$store.commit(`audits/${this.audit_id}/informationRequests/DELETE_CRITERIA_PERIOD`, { criteria, period });
+        },
+
+        onCriteriaPeriodEdit(period) {
+            this.selectedPeriodStartDate = period.data.start_date;
+            this.selectedPeriodEndDate = period.data.end_date;
+
+            // this.$store.dispatch(`audits/${this.audit_id}/informationRequests/cancelEditCriteria`);
+            this.showAddNewPeriodButton = false;
+            period.uiState.edit = true;
+        },
+
         onCriteriaDateChange(date) {
             this.selectedCriteriaDate = date;
         },
+
+        onPeriodStartDateChange(date) {
+            this.selectedPeriodStartDate = date;
+        },
+
+        onPeriodEndDateChange(date) {
+            this.selectedPeriodEndDate = date;
+        },
+
         onSaveCriteria() {                
             this.savingCriteria = true;
             setTimeout(() => {
@@ -201,8 +321,41 @@ export default {
                 // });
             }, 1000);
         },
+
+        onSaveCriteriaPeriod(criteria) {
+            this.savingPeriod = true;
+            setTimeout(() => {
+                this.$store.commit(
+                    `audits/${this.audit_id}/informationRequests/SAVE_CRITERIA_PERIOD`, 
+                    {
+                        criteria,
+                        period: {
+                            start_date: this.selectedPeriodStartDate,
+                            end_date: this.selectedPeriodEndDate
+                        }
+                    }
+                );
+                this.selectedPeriodStartDate = null;
+                this.selectedPeriodEndDate = null;
+                this.savingPeriod = false;
+
+                this.showAddNewPeriodButton = true;
+
+                // this.$nextTick(() => {
+                //     this.$refs.addNewButton.$el.focus();
+                // });
+            }, 1000);
+        },
+
         onSelectCriteria(criteria) {
             this.selectedCriteriaOption = criteria;
+            this.$nextTick(() => {
+                if(criteria.value.requiresDate) {
+                    this.$refs.criteriaDatePicker[0].focus();
+                } else {
+                    this.$refs.saveCriteriaButton[0].$el.focus();
+                }
+            });
         }
     }
 }
