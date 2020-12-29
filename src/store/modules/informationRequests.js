f1cda33883af28b1e25ca8c8350978a1015e837b
sebastian thielke <seb@firstnoodle.io>
2020-12-29 22:20:49 +0100

    informationRequests module → UPDATE_CRITERIAS mutation

--- src/store/modules/informationRequests.js
+++ src/store/modules/informationRequests.js
@@ -207,6 +207,9 @@ export default {
                     }
                 }
             )
+        },
+        UPDATE_CRITERIAS(state, criterias) {
+            state.criterias = criterias;
         }
     },
     state: () => ({
