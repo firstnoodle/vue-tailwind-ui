d532f88a786d59ea3629990de059fc1da1a6f777
sebastian thielke <seb@firstnoodle.io>
2020-12-30 23:22:48 +0100

    distribution module → crud for plan and report dates

--- src/store/modules/distribution.js
+++ src/store/modules/distribution.js
@@ -3,12 +3,49 @@ import Vue from 'vue';
 export default {
     namespaced: true,
     actions: {
-
+        cancelEditPlanDate({state, commit}) {
+            if(!state.plan.date.id) {
+                commit('DELETE_PLAN_DATE');
+            } else {
+                commit('CANCEL_EDIT_PLAN_DATE');
+            }
+        },
+        cancelEditReportDate({state, commit}) {
+            if(!state.report.date.id) {
+                commit('DELETE_REPORT_DATE');
+            } else {
+                commit('CANCEL_EDIT_REPORT_DATE');
+            }
+        }
     },
-    getters: {
-
-    },
+    getters: {},
     mutations: {
+        ADD_PLAN_DATE(state) {
+            state.plan.date = {
+                id: null,
+                data: {
+                    date: null
+                },
+                uiState: {
+                    edit: true,
+                    listId: Date.now(),
+                    selected: false
+                }
+            };
+        },
+        ADD_REPORT_DATE(state) {
+            state.report.date = {
+                id: null,
+                data: {
+                    date: null
+                },
+                uiState: {
+                    edit: true,
+                    listId: Date.now(),
+                    selected: false
+                }
+            };
+        },
         ADD_RECIPIENT(state, target) {
             if(target in state) {
                 state[target].recipients.push({
@@ -24,12 +61,52 @@ export default {
                 })
             }
         },
+        CANCEL_EDIT_PLAN_DATE(state) {
+            state.plan.date.uiState.edit = false;
+        },
+        CANCEL_EDIT_REPORT_DATE(state) {
+            state.report.date.uiState.edit = false;
+        },
+        DELETE_PLAN_DATE(state) {
+            state.plan.date = null;
+        },
+        DELETE_REPORT_DATE(state) {
+            state.report.date = null;
+        },
         DELETE_RECIPIENT(state, {target, recipient}) {
             if(target in state) {
                 Vue.delete(
                     state[target].recipients,
                     state[target].recipients.findIndex(r => r.id === recipient.id)
                 );
+            }
+        },
+        EDIT_PLAN_DATE(state) {
+            state.plan.date.uiState.edit = true;
+        },
+        EDIT_REPORT_DATE(state) {
+            state.report.date.uiState.edit = true;
+        },
+        SAVE_PLAN_DATE(state, date) {
+            state.plan.date = {
+                id: state.plan.date.id || Date.now(),
+                data: { date },
+                uiState: {
+                    edit: false,
+                    listId: state.plan.date.uiState.listId,
+                    selected: false
+                }
+            }
+        },
+        SAVE_REPORT_DATE(state, date) {
+            state.report.date = {
+                id: state.report.date.id || Date.now(),
+                data: { date },
+                uiState: {
+                    edit: false,
+                    listId: state.report.date.uiState.listId,
+                    selected: false
+                }
             }
         },
         SAVE_RECIPIENT(state, {target, recipient}) {
