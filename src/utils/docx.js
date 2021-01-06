b0e5575cd3100fc3fc6beed18ffdf807129f7667
sebastian thielke <seb@firstnoodle.io>
2021-01-06 12:29:04 +0100

    utils/docx → basic text styles in place + exportPlan method

--- src/utils/docx.js
+++ src/utils/docx.js
@@ -3,6 +3,35 @@ import { saveAs } from 'file-saver';
 
 const FONT = 'apis';
 
+export const exportPlan = () => {
+    const doc = new Document({ 
+        title: 'Audit Plan',
+        styles 
+    });
+
+    // front page
+    doc.addSection({
+        children: [
+            new Paragraph({
+                style: 'documentTitle',
+                text: 'ISO/GMP AUDIT PLAN'
+            }),
+            new Paragraph({
+                style: 'heading',
+                text: 'Routine audit of'
+            }),
+            new Paragraph({
+                style: 'body',
+                text: 'testing'
+            }),
+        ]
+    });
+
+    Packer.toBlob(doc).then(blob => {
+        saveAs(blob, "Audit Plan.docx");
+    });
+};
+
 export const exportRequirements = requirements => {
 
     const doc = new Document({ styles });
@@ -35,8 +64,8 @@ export const exportRequirements = requirements => {
 const styles = {
     paragraphStyles: [
         {
-            id: "heading",
-            name: "heading",
+            id: "documentTitle",
+            name: "documentTitle",
             basedOn: "Normal",
             next: "Normal",
             quickFormat: true,
@@ -48,6 +77,19 @@ const styles = {
             paragraph: {},
         },
         {
+            id: "heading",
+            name: "heading",
+            basedOn: "Normal",
+            next: "Normal",
+            quickFormat: true,
+            run: {
+                bold: true,
+                font: FONT,
+                size: 24,
+            },
+            paragraph: {},
+        },
+        {
             id: "body",
             name: "body",
             basedOn: "Normal",
@@ -55,7 +97,7 @@ const styles = {
             quickFormat: true,
             run: {
                 font: FONT,
-                size: 22,
+                size: 20,
             },
             paragraph: {},
         },
